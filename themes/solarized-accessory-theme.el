dcd949400d598f1f6a51e5046f897b35906e83e0
0x60df <0x60df@gmail.com>
2016-08-18 00:01:36 +0900

    Modify themes/solarized-accessory-theme.el
    same as tomorrow-accessory improvement
    modified at before commit

--- themes/solarized-accessory-theme.el
+++ themes/solarized-accessory-theme.el
@@ -9,42 +9,38 @@
 (when (featurep 'auto-complete)
   (custom-theme-set-faces
    'solarized-accessory
-   `(ac-candidate-face
-     ((t :foreground ,(face-foreground 'default)
-         :background ,(face-background 'default))))
-   `(ac-selection-face
-     ((t :foreground ,(face-background 'default)
-         :background ,(face-foreground 'default))))
-   `(ac-completion-face
-     ((t :foreground ,(face-foreground 'font-lock-comment-face)
-         :underline ,(face-foreground 'font-lock-comment-face))))
-   `(popup-tip-face
-     ((t :foreground ,(face-foreground 'mode-line)
-         :background ,(face-background 'mode-line))))
-   `(ac-yasnippet-candidate-face
-     ((t :inherit ac-candidate-face
-         :foreground ,(face-foreground 'font-lock-warning-face))))
-   `(ac-yasnippet-selection-face
-     ((t :inherit ac-selection-face)))
-   `(ac-dictionary-candidate-face
-     ((t :inherit ac-candidate-face
-         :foreground ,(face-foreground 'font-lock-keyword-face))))
-   `(ac-dictionary-selection-face
-     ((t :inherit ac-selection-face)))
-   `(ac-symbols-candidate-face
-     ((t :inherit ac-candidate-face
-         :foreground ,(face-foreground 'font-lock-constant-face))))
-   `(ac-symbols-selection-face
-     ((t :inherit ac-selection-face)))
-   `(ac-variables-candidate-face
-     ((t :inherit ac-candidate-face
-         :foreground ,(face-foreground 'font-lock-variable-name-face))))
-   `(ac-variables-selection-face
-     ((t :inherit ac-selection-face)))
-   `(ac-functions-candidate-face
-     ((t :inherit ac-candidate-face
-         :foreground ,(face-foreground 'font-lock-function-name-face))))
-   `(ac-functions-selection-face
-     ((t :inherit ac-selection-face)))))
+   '(popup-face ((t :inherit default)))
+   '(popup-isearch-match ((t :inherit lazy-highlight)))
+   '(popup-menu-mouse-face ((t :inherit popup-menu-selection-face)))
+   '(popup-menu-selection-face ((t :inherit region)))
+   '(popup-scroll-bar-background-face ((t :inherit mode-line-inactive)))
+   '(popup-scroll-bar-foreground-face ((t :inherit mode-line-inactive
+                                          :inverse-video t)))
+   '(popup-summary-face ((t :inherit shadow)))
+   '(popup-tip-face ((t :inherit mode-line)))
+   '(ac-gtags-candidate-face
+     ((t :inherit (font-lock-function-name-face ac-candidate-face))))
+   '(ac-gtags-selection-face
+     ((t :inherit (font-lock-function-name-face ac-selection-face))))
+   '(ac-yasnippet-candidate-face
+     ((t :inherit (font-lock-warning-face ac-candidate-face))))
+   '(ac-yasnippet-selection-face
+     ((t :inherit (font-lock-warning-face ac-selection-face))))
+   '(ac-dictionary-candidate-face
+     ((t :inherit (font-lock-keyword-face ac-candidate-face))))
+   '(ac-dictionary-selection-face
+     ((t :inherit (font-lock-keyword-face ac-selection-face))))
+   '(ac-symbols-candidate-face
+     ((t :inherit (font-lock-constant-face ac-candidate-face))))
+   '(ac-symbols-selection-face
+     ((t :inherit (font-lock-constant-face ac-selection-face))))
+   '(ac-variables-candidate-face
+     ((t :inherit (font-lock-variable-name-face ac-candidate-face))))
+   '(ac-variables-selection-face
+     ((t :inherit (font-lock-variable-name-face ac-selection-face))))
+   '(ac-functions-candidate-face
+     ((t :inherit (font-lock-function-name-face ac-candidate-face))))
+   '(ac-functions-selection-face
+     ((t :inherit (font-lock-function-name-face ac-selection-face))))))
 
 (provide-theme 'solarized-accessory)
