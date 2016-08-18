49cf66db05694a105cab5d997a335b4857184dc0
0x60df <0x60df@gmail.com>
2016-08-18 23:52:53 +0900

    Modify tomorrow-accessory-theme.el
    simplify setting about mode line

--- themes/tomorrow-accessory-theme.el
+++ themes/tomorrow-accessory-theme.el
@@ -9,14 +9,11 @@
 (custom-theme-set-faces
  'tomorrow-accessory
  `(mode-line-buffer-identification-face
-   ((t :foreground ,(face-foreground 'font-lock-keyword-face)
+   ((t :inherit (font-lock-keyword-face mode-line-buffer-id)
        :weight bold)))
- `(mode-line-vc-mode-face
-   ((t :foreground ,(face-foreground 'font-lock-variable-name-face))))
- `(mode-line-mode-name-face
-   ((t :foreground ,(face-foreground 'font-lock-type-face))))
- `(mode-line-which-func-mode-face
-   ((t :foreground ,(face-foreground 'font-lock-function-name-face)))))
+ `(mode-line-vc-mode-face ((t :inherit font-lock-variable-name-face)))
+ `(mode-line-mode-name-face ((t :inherit font-lock-type-face)))
+ `(mode-line-which-func-mode-face ((t :inherit font-lock-function-name-face))))
 
 
 ;;; popup
