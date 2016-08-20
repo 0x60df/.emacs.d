dd752545572faa4d46b4e97747af580c086cb27d
0x60df <0x60df@gmail.com>
2016-08-21 02:59:03 +0900

    Add tomorrow-patch-theme.el
    for highlight face

---
+++ themes/tomorrow-patch-theme.el
@@ -0,0 +1,11 @@
+
+;;;; tomorrow-patch-theme.el
+
+
+(deftheme tomorrow-patch)
+
+(custom-theme-set-faces
+ 'tomorrow-patch
+ `(highlight ((t :foreground ,(face-background 'default)))))
+
+(provide-theme 'tomorrow-patch)
