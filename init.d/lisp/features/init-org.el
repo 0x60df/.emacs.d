202dc0e149d11a1ddaf99f4152c068108a570adf
0x60df <0x60df@gmail.com>
2016-08-21 23:54:14 +0900

    Modify binding for org
    C-c @ for multiple-cursors

--- init.d/lisp/features/init-org.el
+++ init.d/lisp/features/init-org.el
@@ -13,3 +13,11 @@
      (define-key org-mode-map (kbd "C-,") 'other-window)
      (define-key org-mode-map (kbd "C-c ;") 'manipulate-frame)
      (define-key org-mode-map (kbd "C-c :") 'manipulate-window)))
+(add-hook 'org-mode-hook
+          (lambda ()
+            (when (fboundp 'multiple-cursors-mode)
+              (local-unset-key (kbd "C-c @"))
+              (local-set-key (kbd "C-c @ e") 'mc/edit-lines)
+              (local-set-key (kbd "C-c @ n") 'mc/mark-next-like-this)
+              (local-set-key (kbd "C-c @ p") 'mc/mark-previous-like-this)
+              (local-set-key (kbd "C-c @ a") 'mc/mark-all-like-this))))
