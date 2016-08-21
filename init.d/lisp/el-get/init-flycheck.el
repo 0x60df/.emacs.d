2faeb84881a1f6f4059918d74c81df25375c3750
0x60df <0x60df@gmail.com>
2016-08-22 00:53:56 +0900

    Modify flycheck
    turn off global-flycheck-mode
    add binding for flycheck-mode
    disable-checker emacs-lisp-checkdoc

--- init.d/lisp/el-get/init-flycheck.el
+++ init.d/lisp/el-get/init-flycheck.el
@@ -2,4 +2,8 @@
 ;;;; init-flycheck.el
 
 
-(global-flycheck-mode)
+(global-set-key (kbd "C-c !") 'flycheck-mode)
+(eval-after-load 'flycheck
+  '(progn
+     (define-key flycheck-mode-map (kbd "C-c ! !") 'flycheck-mode)
+     (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))))
