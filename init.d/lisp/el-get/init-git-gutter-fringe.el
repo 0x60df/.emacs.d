39c0e063c23368c7dbad394f823c4b0435c3dcfa
0x60df <0x60df@gmail.com>
2016-08-25 23:52:44 +0900

    Modify git-gutter-fringe
    add bindings
      - p previous-hunk
      - n next-hunk
      - d popup

--- init.d/lisp/el-get/init-git-gutter-fringe.el
+++ init.d/lisp/el-get/init-git-gutter-fringe.el
@@ -14,7 +14,9 @@
 
 (global-set-key "\C-cv<" 'git-gutter:previous-hunk)
 (global-set-key "\C-cv>" 'git-gutter:next-hunk)
-(global-set-key "\C-cvp" 'git-gutter:popup-hunk)
+(global-set-key "\C-cvp" 'git-gutter:previous-hunk)
+(global-set-key "\C-cvn" 'git-gutter:next-hunk)
+(global-set-key "\C-cvd" 'git-gutter:popup-hunk)
 (global-set-key "\C-cvr" 'git-gutter:revert-hunk)
 (global-set-key "\C-cvs" 'git-gutter:stage-hunk)
 (global-set-key "\C-cv " 'git-gutter:mark-hunk)
