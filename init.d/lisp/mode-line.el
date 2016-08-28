11fb194279a202b3d9620653f76ba336e05106ca
0x60df <0x60df@gmail.com>
2016-08-28 23:09:22 +0900

    Modiy mode-line-format
    old: hard coded on mode-line-format
    new: coded on each mode-line variables

--- init.d/lisp/mode-line.el
+++ init.d/lisp/mode-line.el
@@ -5,41 +5,59 @@
 
 ;;; format
 
+(setq-default mode-line-mule-info '("%Z"))
+(setq mode-line-client '(:eval (if (frame-parameter nil 'client) "@" " ")))
+(setq-default mode-line-modified '("%1*%1+"))
+(setq-default mode-line-remote '("%1@"))
+(setq mode-line-frame-identification
+              '((window-system
+                 (-2 (:eval (number-to-string (length (frame-list)))))
+                 "%F")
+                " "))
+(setq-default mode-line-buffer-identification
+      '(:propertize "%12b" face mode-line-buffer-identification-face))
+(setq mode-line-position
+      '(:propertize ((-3 "%p")
+                     (size-indication-mode (5 "/%I"))
+                     (line-number-mode ((column-number-mode (7  " %l %c")
+                                                            (4  " L%l")))
+                                       ((column-number-mode (4  " C%c")))))
+                    face mode-line-position-face))
+(setq mode-line-modes
+      '("%["
+        ((:propertize mode-name face mode-line-mode-name-face)
+         mode-line-process
+         (:propertize minor-mode-alist face mode-line-minor-mode-alist-face)
+         "%n")
+        "%]"
+        " "))
+(setq mode-line-misc-info
+      '((which-func-mode
+         (""
+          (:propertize which-func-current face mode-line-which-func-mode-face)
+          " "))
+        (global-mode-string ("" global-mode-string " "))))
+
 (setq-default
  mode-line-format
- '("%e"
-   mode-line-front-space
-   "%Z"
-   " "
-   "%1*%1+"
-   "%1@"
-   (window-system (-2 (:eval (number-to-string (length (frame-list))))) "%F")
-   " "
-   (:propertize "%12b" face mode-line-buffer-identification-face)
-   " "
-   (:propertize ((-3 "%p")
-                 (size-indication-mode (5 "/%I"))
-                 (line-number-mode ((column-number-mode (7  " %l %c")
-                                                        (4  " L%l")))
-                                   ((column-number-mode (4  " C%c")))))
-                face mode-line-position-face)
-   (vc-mode (" " (:eval (propertize (replace-regexp-in-string
-                                     "^\\s-+\\|\\s-+$" ""
-                                     (substring-no-properties vc-mode))
-                                    'face 'mode-line-vc-mode-face))))
-   " "
-   "%["
-   (:propertize mode-name face mode-line-mode-name-face)
-   mode-line-process
-   (:propertize minor-mode-alist face mode-line-minor-mode-alist-face)
-   "%n"
-   "%]"
-   (which-func-mode
-    (" "
-     (:propertize which-func-current face mode-line-which-func-mode-face)
-     " "))
-   (global-mode-string ("" global-mode-string " "))
-   mode-line-end-spaces))
+ (letrec ((replace-element
+           (lambda (o n l)
+             (cond ((null l) l)
+                   ((equal (car l) o) (cons n (cdr l)))
+                   (t (cons (car l)
+                            (funcall replace-element o n (cdr l))))))))
+   (funcall replace-element
+            '(vc-mode vc-mode)
+            '(vc-mode (" " (:propertize
+                            (:eval (replace-regexp-in-string
+                                    "^\\s-+\\|\\s-+$" ""
+                                    (substring-no-properties vc-mode)))
+                            face mode-line-vc-mode-face)))
+            (mapcar
+             (lambda (e) (if (stringp e)
+                             (replace-regexp-in-string "^\\s-+$" " " e)
+                           e))
+             mode-line-format))))
 
 
 ;;; faces
