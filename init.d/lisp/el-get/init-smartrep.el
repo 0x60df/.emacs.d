cc54653753b26802409f628a13068723ecc513f5
0x60df <0x60df@gmail.com>
2016-08-28 23:17:49 +0900

    Modify smartrep
    when featurep -> eval-after-load about git-getter

--- init.d/lisp/el-get/init-smartrep.el
+++ init.d/lisp/el-get/init-smartrep.el
@@ -10,6 +10,8 @@
 (custom-set-variables '(smartrep-mode-line-string-activated ""))
 
 
+;;; bindings
+
 ;; window
 (smartrep-define-key
  global-map "C-x"
@@ -27,30 +29,25 @@
  '(("0" . delete-frame)
    ("o" . other-frame)))
 
-
-;;; org
-
+;; org
 (eval-after-load 'org
-  '(progn
-     (smartrep-define-key
-         org-mode-map "C-c"
-       '(("C-n" . (lambda () (outline-next-visible-heading 1)))
-         ("C-p" . (lambda () (outline-previous-visible-heading 1)))
-         ("C-f" . (lambda () (org-forward-heading-same-level 1)))
-         ("C-b" . (lambda () (org-backward-heading-same-level 1)))
-         ("C-^" . org-up-element)
-         ("C-_" . org-down-element)))))
+  '(smartrep-define-key
+       org-mode-map "C-c"
+     '(("C-n" . (lambda () (outline-next-visible-heading 1)))
+       ("C-p" . (lambda () (outline-previous-visible-heading 1)))
+       ("C-f" . (lambda () (org-forward-heading-same-level 1)))
+       ("C-b" . (lambda () (org-backward-heading-same-level 1)))
+       ("C-^" . org-up-element)
+       ("C-_" . org-down-element))))
 
-
-;;; git-gutter
-
-(when (featurep 'git-gutter)
-  (smartrep-define-key
-      global-map "C-c v"
-    '(("<" . git-gutter:previous-hunk)
-      (">" . git-gutter:next-hunk)
-      ("p" . git-gutter:previous-hunk)
-      ("n" . git-gutter:next-hunk)
-      ("d" . git-gutter:popup-hunk)
-      ("r" . git-gutter:revert-hunk)
-      ("s" . git-gutter:stage-hunk))))
+;; git-gutter
+(eval-after-load 'git-gutter
+  '(smartrep-define-key
+       global-map "C-c v"
+     '(("<" . git-gutter:previous-hunk)
+       (">" . git-gutter:next-hunk)
+       ("p" . git-gutter:previous-hunk)
+       ("n" . git-gutter:next-hunk)
+       ("d" . git-gutter:popup-hunk)
+       ("r" . git-gutter:revert-hunk)
+       ("s" . git-gutter:stage-hunk))))
