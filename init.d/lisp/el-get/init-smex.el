53c8fc3c5c8455e96189dbc980f4a4b8893752af
0x60df <0x60df@gmail.com>
2016-09-04 01:47:33 +0900

    Modify smex biding caused by ido settiong change

--- init.d/lisp/el-get/init-smex.el
+++ init.d/lisp/el-get/init-smex.el
@@ -13,3 +13,9 @@
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 (global-set-key (kbd "M-s-x") 'execute-extended-command)
+
+(defadvice smex-prepare-ido-bindings (after
+                                      smex-prepare-additional-ido-bindings)
+  (define-key ido-completion-map " " 'ido-next-match)
+  (define-key ido-completion-map (kbd "S-SPC") 'ido-prev-match))
+(ad-activate 'smex-prepare-ido-bindings)
