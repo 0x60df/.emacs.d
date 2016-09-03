1d8a7c37aa010d82a02354b09240299cae3595db
0x60df <0x60df@gmail.com>
2016-09-03 22:07:50 +0900

    Improve ido stop auto merge

--- init.d/lisp/features/init-ido.el
+++ init.d/lisp/features/init-ido.el
@@ -2,14 +2,25 @@
 ;;;; init-ido.el
 
 
+
+;;; base
+
 (ido-mode t)
 (require 'ido)
+(setq ido-everywhere t)
 (setq ido-enable-flex-matching t)
+(setq ido-auto-merge-work-directories-length -1)
 
 (add-hook 'ido-minibuffer-setup-hook
           (lambda ()
             (set (make-local-variable 'truncate-lines) t)))
-(add-hook 'ido-setup-hook
-          (lambda ()
-            (define-key ido-completion-map " " 'ido-next-match)
-            (define-key ido-completion-map (kbd "S-SPC") 'ido-prev-match)))
+
+
+;;; bindings
+
+(define-key ido-common-completion-map " " #'ido-next-match)
+(define-key ido-common-completion-map (kbd "S-SPC") #'ido-prev-match)
+(define-key ido-file-dir-completion-map (kbd "C-<tab>")
+  (lambda()
+    (interactive)
+    (ido-initiate-auto-merge (current-buffer))))
