c0f2a56dd87f857d127e8dabf30b277db4fff887
0x60df <0x60df@gmail.com>
2016-09-04 15:11:04 +0900

    Add some key bind for smartparens

--- init.d/lisp/el-get/init-smartparens.el
+++ init.d/lisp/el-get/init-smartparens.el
@@ -2,8 +2,26 @@
 ;;;; smartparens.el
 
 
+
+;;; base
+
 (smartparens-global-mode)
 (sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
 (sp-local-pair 'lisp-interaction-mode "'" nil :actions nil)
 (sp-local-pair 'emacs-lisp-mode "`" nil :actions nil)
 (sp-local-pair 'lisp-interaction-mode "`" nil :actions nil)
+
+;;; bindings
+
+(global-set-key (kbd "C-(") (lambda (arg)
+                              (interactive "P")
+                              (if arg
+                                  (sp-backward-slurp-sexp)
+                                (sp-forward-barf-sexp))))
+(global-set-key (kbd "C-)") (lambda (arg)
+                              (interactive "P")
+                              (if arg
+                                  (sp-backward-barf-sexp)
+                                (sp-forward-slurp-sexp))))
+(global-set-key (kbd "s-(") 'sp-splice-sexp)
+(global-set-key (kbd "s-)") 'sp-rewrap-sexp)
