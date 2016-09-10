58413d90e8c661c5aee6b928360fbb9ee8d72b45
0x60df <0x60df@gmail.com>
2016-09-11 02:15:27 +0900

    Modify server setting
    change function name
    
    add hook for ad-activate
    
    define macro for registering code fragment
    as delayed eval group on daemon

--- init.d/lisp/features/init-server.el
+++ init.d/lisp/features/init-server.el
@@ -6,10 +6,19 @@
   (add-hook 'server-visit-hook
             (lambda () (keyboard-translate ?\C-h ?\C-?)))
 
-  (defun server-init-daemon-at-first-make-frame (frame)
+  (defun server-after-first-make-frame-function (frame)
     (select-frame frame)
     (remove-hook 'after-make-frame-functions
-                 'server-init-daemon-at-first-make-frame))
+                 'server-after-first-make-frame-function))
+
+  (add-hook 'after-init-hook
+            (lambda () (ad-activate 'server-after-first-make-frame-function)))
 
   (add-hook 'after-make-frame-functions
-            'server-init-daemon-at-first-make-frame))
+            'server-after-first-make-frame-function))
+
+(defmacro server-eval-after-first-make-frame (name &rest body)
+  `(cond ((daemonp)
+          (defadvice server-after-first-make-frame-function (after ,name)
+            ,@body))
+         (t ,@body)))
