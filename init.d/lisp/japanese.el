8e0f6cf9b0125b42b8e0e07b6fcd7a28b512c2d7
0x60df <0x60df@gmail.com>
2016-09-19 00:30:14 +0900

    Modify japanese.el register zenkaku whitespace
    for trailing-whitespace on emacs-lisp-mode

--- init.d/lisp/japanese.el
+++ init.d/lisp/japanese.el
@@ -4,3 +4,4 @@
 
 (font-lock-add-keywords nil '(("　" . 'trailing-whitespace)))
 (font-lock-add-keywords 'c-mode '(("　" . 'trailing-whitespace)))
+(font-lock-add-keywords 'emacs-lisp-mode '(("　" . 'trailing-whitespace)))
