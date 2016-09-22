2a237b74c4c305e05541ffee383c245b4113c069
0x60df <0x60df@gmail.com>
2016-09-22 14:57:58 +0900

    Modify trailing white space
    disable on some modes

--- init.d/lisp/el-get/init-magit.el
+++ init.d/lisp/el-get/init-magit.el
@@ -7,3 +7,8 @@
 
 (global-set-key "\C-cvm" 'magit-status)
 (global-set-key "\C-cvc" 'magit-checkout)
+(mapc (lambda (hook)
+        (add-hook hook (lambda ()
+                         (setq show-trailing-whitespace nil))))
+      '(magit-mode-hook
+        magit-popup-mode-hook))
--- init.d/lisp/whitespace.el
+++ init.d/lisp/whitespace.el
@@ -3,3 +3,8 @@
 
 
 (setq-default show-trailing-whitespace t)
+(mapc (lambda (hook)
+        (add-hook hook (lambda ()
+                         (setq show-trailing-whitespace nil))))
+      '(term-mode-hook
+        eshell-mode-hook))
