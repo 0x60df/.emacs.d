d918a3b70b97555e4681602927b90b3317e4d652
0x60df <0x60df@gmail.com>
2016-10-30 03:43:30 +0900

    Modify init-ddskk.el
    adopt change of variable name for ccc

--- init.d/lisp/el-get/init-ddskk.el
+++ init.d/lisp/el-get/init-ddskk.el
@@ -2,11 +2,17 @@
 ;;;; init-skk.el
 
 
+
+;;; base
+
 (premise init "~/.emacs.d/init.el")
 (premise frame)
 (premise client)
 
 (require 'skk-autoloads)
+
+
+;;; bindings
 
 (global-set-key "\C-x\C-j" 'skk-mode)
 (global-set-key (kbd "s-\\") 'skk-mode)
@@ -24,104 +30,126 @@
               (kbd "C-j"))))
 (ad-activate 'skk-mode)
 
+
+;;; multiple terminal enviroment
+
 (defadvice skk-cursor-set-1 (around get-terminal-cursor-color)
-  (let ((default-cursor-color (terminal-parameter nil 'terminal-cursor-color)))
+  (let ((ccc-default-cursor-color
+         (terminal-parameter nil 'ccc-terminal-cursor-color)))
     ad-do-it))
 (defadvice skk-cursor-off-1 (around get-terminal-cursor-color)
-  (let ((default-cursor-color (terminal-parameter nil 'terminal-cursor-color)))
+  (let ((ccc-default-cursor-color
+         (terminal-parameter nil 'ccc-terminal-cursor-color)))
     ad-do-it))
-(defadvice default-cursor-color (around get-terminal-cursor-color)
-  (let ((default-cursor-color (terminal-parameter nil 'terminal-cursor-color)))
+(defadvice ccc-default-cursor-color (around get-terminal-cursor-color)
+  (let ((ccc-default-cursor-color
+         (terminal-parameter nil 'ccc-terminal-cursor-color)))
     ad-do-it))
 (defadvice ccc-setup-new-frame (around get-terminal-cursor-color)
-  (let ((default-cursor-color
-          (terminal-parameter frame 'terminal-cursor-color)))
+  (let ((ccc-default-cursor-color
+          (terminal-parameter frame 'ccc-terminal-cursor-color)))
     ad-do-it))
 (defadvice ccc-setup (around get-terminal-cursor-color)
-  (setq default-cursor-color
-        (let ((default-cursor-color
-                (terminal-parameter nil 'terminal-cursor-color)))
+  (setq ccc-default-cursor-color
+        (let ((ccc-default-cursor-color
+                (terminal-parameter nil 'ccc-terminal-cursor-color)))
           ad-do-it
-          default-cursor-color)))
+          ccc-default-cursor-color)))
 
 (defadvice ccc-setup (after set-terminal-cursor-color)
-  (set-terminal-parameter nil 'terminal-cursor-color default-cursor-color))
+  (set-terminal-parameter
+   nil 'ccc-terminal-cursor-color ccc-default-cursor-color))
 (defadvice ccc-setup-current-colors (after set-terminal-cursor-color)
-  (set-terminal-parameter nil 'terminal-cursor-color default-cursor-color))
+  (set-terminal-parameter
+   nil 'ccc-terminal-cursor-color ccc-default-cursor-color))
 (defadvice custom-theme-checkbox-toggle (after set-terminal-cursor-color)
-  (set-terminal-parameter nil 'terminal-cursor-color default-cursor-color))
+  (set-terminal-parameter
+   nil 'ccc-terminal-cursor-color ccc-default-cursor-color))
 
-(defadvice default-foreground-color (around get-terminal-foreground-color)
-  (let ((default-foreground-color
-          (terminal-parameter nil 'terminal-foreground-color)))
+(defadvice ccc-default-foreground-color (around get-terminal-foreground-color)
+  (let ((ccc-default-foreground-color
+         (terminal-parameter nil 'ccc-terminal-foreground-color)))
     ad-do-it))
 (defadvice ccc-setup-new-frame (around get-terminal-foreground-color)
-  (let ((default-foreground-color
-          (terminal-parameter frame 'terminal-foreground-color)))
+  (let ((ccc-default-foreground-color
+          (terminal-parameter frame 'ccc-terminal-foreground-color)))
     ad-do-it))
 (defadvice ccc-setup (around get-terminal-foreground-color)
-  (setq default-foreground-color
-        (let ((default-foreground-color
-                (terminal-parameter nil 'terminal-foreground-color)))
+  (setq ccc-default-foreground-color
+        (let ((ccc-default-foreground-color
+               (terminal-parameter nil 'ccc-terminal-foreground-color)))
           ad-do-it
-          default-foreground-color)))
+          ccc-default-foreground-color)))
 
 (defadvice ccc-setup (after set-terminal-foreground-color)
   (set-terminal-parameter
-   nil 'terminal-foreground-color default-foreground-color))
+   nil 'ccc-terminal-foreground-color ccc-default-foreground-color))
 (defadvice ccc-setup-current-colors (after set-terminal-foreground-color)
   (set-terminal-parameter
-   nil 'terminal-foreground-color default-foreground-color))
+   nil 'ccc-terminal-foreground-color ccc-default-foreground-color))
 (defadvice custom-theme-checkbox-toggle (after set-terminal-foreground-color)
   (set-terminal-parameter
-   nil 'terminal-foreground-color default-foreground-color))
+   nil 'ccc-terminal-foreground-color ccc-default-foreground-color))
 
-(defadvice default-background-color (around get-terminal-background-color)
-  (let ((default-background-color
-         (terminal-parameter nil 'terminal-background-color)))
+(defadvice ccc-default-background-color (around get-terminal-background-color)
+  (let ((ccc-default-background-color
+         (terminal-parameter nil 'ccc-terminal-background-color)))
     ad-do-it))
 (defadvice ccc-setup-new-frame (around get-terminal-background-color)
-  (let ((default-background-color
-         (terminal-parameter frame 'terminal-background-color)))
+  (let ((ccc-default-background-color
+         (terminal-parameter frame 'ccc-terminal-background-color)))
     ad-do-it))
 (defadvice ccc-setup (around get-terminal-background-color)
-  (setq default-background-color
-        (let ((default-background-color
-                (terminal-parameter nil 'terminal-background-color)))
+  (setq ccc-default-background-color
+        (let ((ccc-default-background-color
+                (terminal-parameter nil 'ccc-terminal-background-color)))
           ad-do-it
-          default-background-color)))
+          ccc-default-background-color)))
 
 (defadvice ccc-setup (after set-terminal-background-color)
   (set-terminal-parameter
-   nil 'terminal-background-color default-background-color))
+   nil 'ccc-terminal-background-color ccc-default-background-color))
 (defadvice ccc-setup-current-colors (after set-terminal-background-color)
   (set-terminal-parameter
-   nil 'terminal-background-color default-background-color))
+   nil 'ccc-terminal-background-color ccc-default-background-color))
 (defadvice custom-theme-checkbox-toggle (after set-terminal-background-color)
   (set-terminal-parameter
-   nil 'terminal-background-color default-background-color))
+   nil 'ccc-terminal-background-color ccc-default-background-color))
 
 (mapc (lambda (f) (ad-activate f))
       '(skk-cursor-set-1
         skk-cursor-off-1
-        default-cursor-color
+        ccc-default-cursor-color
         ccc-setup-new-frame
         ccc-setup
         ccc-setup-current-colors
         custom-theme-checkbox-toggle
-        default-foreground-color
-        default-background-color))
+        ccc-default-foreground-color
+        ccc-default-background-color))
 
 (autoload 'ccc-setup-current-colors "ccc")
 (autoload 'ccc-setup-new-frame "ccc")
 
 (defun ccc-setup-new-terminal-function ()
+  (add-hook 'post-command-hook 'ccc-update-buffer-local-frame-params)
+  (add-hook 'after-make-frame-functions 'ccc-setup-new-frame)
   (ccc-setup-current-colors)
   (ccc-setup-new-frame (selected-frame))
   (remove-hook 'pre-command-hook #'ccc-setup-new-terminal-function))
 (add-hook 'after-make-terminal-functions
           (lambda (terminal)
             (add-hook 'pre-command-hook #'ccc-setup-new-terminal-function)))
+
+
+;;; server
+
+(defadvice server-create-window-system-frame
+    (after ccc-update-buffer-local-frame-params)
+  (ccc-update-buffer-local-frame-params))
+(ad-activate 'server-create-window-system-frame)
+
+
+;;; evil
 
 (eval-after-load 'skk
   '(eval-after-load 'evil
