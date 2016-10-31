01e01a201da9e02d4b21a1e8155ba4608a2c5637
0x60df <0x60df@gmail.com>
2016-10-31 23:16:02 +0900

    Modify init-multiple-cursors.el
    add key bind C-c @ SPC as set-mark-command
    because C-SPC does not call set-mark-command but mc/mark-all-dwim in console.
    This behavior caused by key code for C-SPC which is equivalent to C-@.

--- init.d/lisp/el-get/init-multiple-cursors.el
+++ init.d/lisp/el-get/init-multiple-cursors.el
@@ -13,6 +13,7 @@
 (global-set-key (kbd "C-c @ n") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-c @ p") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c @ a") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-c @ SPC") 'set-mark-command)
 
 (eval-after-load 'org
   '(define-key org-mode-map (kbd "C-c @") nil))
