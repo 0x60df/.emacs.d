3292aa87b4488ba9c446dc53d77237a8e0fe9ab2
0x60df <0x60df@gmail.com>
2016-10-31 23:14:57 +0900

    Modify init-multi-term.el
    modify some key bindings (e.g. C-r term-send-reverse-search-history)

--- init.d/lisp/el-get/init-multi-term.el
+++ init.d/lisp/el-get/init-multi-term.el
@@ -3,3 +3,13 @@
 
 
 (global-set-key (kbd "H-t") 'multi-term)
+(custom-set-variables
+ `(term-bind-key-alist ',(append term-bind-key-alist
+                                 '(("C-p" . term-send-up)
+                                   ("C-n" . term-send-down)
+                                   ("M-p" . previous-line)
+                                   ("M-n" . next-line)
+                                   ("C-s" . nil)
+                                   ("C-r" . term-send-reverse-search-history)
+                                   ("M-s" . isearch-forward)
+                                   ("M-r" . isearch-backward)))))
