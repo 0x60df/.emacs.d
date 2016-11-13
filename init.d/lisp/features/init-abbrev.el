3c7ec03eed09630d9e3058bc7355d94331dabd30
0x60df <0x60df@gmail.com>
2016-11-14 01:01:31 +0900

    Modify setting about abbrev
    ignore abbrev_defs
    
    regard abbrev as site local.
    feel free to add abbrev.
    
    local
    abbrev: words and phrases
    
    global
    ac-dict: words
    yasnippet: snippet including phrases

--- init.d/lisp/features/init-abbrev.el
+++ init.d/lisp/features/init-abbrev.el
@@ -2,5 +2,8 @@
 ;;;; init-abbrev.el
 
 
-(custom-set-variables '(abbrev-file-name "~/.emacs.d/abbrev_defs"))
-(quietly-read-abbrev-file)
+(custom-set-variables
+ '(abbrev-file-name "~/.emacs.d/abbrev_defs")
+ '(save-abbrevs 'silently))
+
+(if (file-readable-p abbrev-file-name) (quietly-read-abbrev-file))
