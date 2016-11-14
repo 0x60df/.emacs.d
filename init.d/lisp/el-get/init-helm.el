19dc520bf144ae306d037b2ca1bf310b151dc59c
0x60df <0x60df@gmail.com>
2016-11-14 22:57:55 +0900

    Modify helm bindings

--- init.d/lisp/el-get/init-helm.el
+++ init.d/lisp/el-get/init-helm.el
@@ -5,16 +5,18 @@
 
 ;;; base
 
-(add-hook 'helm-mode-hook (lambda () (icomplete-mode -1)))
 (setq helm-completion-mode-string " H")
 
 
 ;;; bindings
 
 (custom-set-variables '(helm-command-prefix-key "C-q"))
-(define-key helm-command-map (kbd "C-q") 'quoted-insert)
-(define-key helm-command-map (kbd "C-m") 'helm-mini)
-(define-key helm-command-map (kbd "C-f") 'helm-find-files)
-(define-key helm-command-map (kbd "C-b") 'helm-buffers-list)
-(define-key helm-command-map (kbd "M-x") 'helm-M-x)
-(define-key helm-command-map (kbd "C-o") 'helm-occur)
+(define-key helm-command-map (kbd "C-q") #'quoted-insert)
+(define-key helm-command-map (kbd "C-m") #'helm-mini)
+(define-key helm-command-map (kbd "C-f") #'helm-find-files)
+(define-key helm-command-map (kbd "C-b") #'helm-buffers-list)
+(define-key helm-command-map (kbd "M-x") #'helm-M-x)
+(define-key helm-command-map (kbd "C-o") #'helm-occur)
+
+(define-key helm-map (kbd "TAB") #'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-j") #'helm-select-action)
