5085d3e06347490d08181c072f4aed466d55d260
0x60df <0x60df@gmail.com>
2016-11-19 21:41:01 +0900

    Add some bindings for helm

--- init.d/lisp/el-get/init-helm.el
+++ init.d/lisp/el-get/init-helm.el
@@ -15,9 +15,12 @@
 (define-key helm-command-map (kbd "C-q") #'quoted-insert)
 (define-key helm-command-map (kbd "C-m") #'helm-mini)
 (define-key helm-command-map (kbd "C-f") #'helm-find-files)
-(define-key helm-command-map (kbd "C-b") #'helm-buffers-list)
+(define-key helm-command-map (kbd "b") #'helm-buffers-list)
 (define-key helm-command-map (kbd "M-x") #'helm-M-x)
 (define-key helm-command-map (kbd "C-o") #'helm-occur)
+(define-key helm-command-map (kbd "C-r") #'helm-register)
+(define-key helm-command-map (kbd "C-b") #'helm-bookmarks)
+(define-key helm-command-map (kbd "C-y") #'helm-show-kill-ring)
 
 (define-key helm-map (kbd "TAB") #'helm-execute-persistent-action)
 (define-key helm-map (kbd "C-j") #'helm-select-action)
