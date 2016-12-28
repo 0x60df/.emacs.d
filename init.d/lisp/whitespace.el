a9f8cf1112cb9f78856c9c7b984950609a459d45
0x60df <0x60df@gmail.com>
2016-12-28 15:05:10 +0900

    Modify whitespace.el
    add calendar-mode to black list for trailing whitespace

--- init.d/lisp/whitespace.el
+++ init.d/lisp/whitespace.el
@@ -4,8 +4,8 @@
 
 (setq-default show-trailing-whitespace t)
 (mapc (lambda (hook)
-        (add-hook hook (lambda ()
-                         (setq show-trailing-whitespace nil))))
+        (add-hook hook (lambda () (setq show-trailing-whitespace nil))))
       '(term-mode-hook
         eshell-mode-hook
-        eww-mode-hook))
+        eww-mode-hook
+        calendar-mode-hook))
