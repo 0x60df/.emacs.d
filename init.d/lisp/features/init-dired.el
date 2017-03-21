a7430f105823d4164f891ef4aaac1ab97b01ef7b
0x60df <0x60df@gmail.com>
2017-03-21 20:44:25 +0900

    Modify setting about dired
    always recursive copy

--- init.d/lisp/features/init-dired.el
+++ init.d/lisp/features/init-dired.el
@@ -3,4 +3,5 @@
 
 
 (eval-after-load 'dired
-  '(custom-set-variables '(dired-dwim-target t)))
+  '(custom-set-variables '(dired-dwim-target t)
+                         '(dired-recursive-copies 'always)))
