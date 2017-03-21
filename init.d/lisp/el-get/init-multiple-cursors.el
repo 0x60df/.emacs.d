d08014bd1bdd2b0ff710ec4811734e328ab913c9
0x60df <0x60df@gmail.com>
2017-03-21 20:52:08 +0900

    Modify setting about multiple-cursors
    disable visible-mark and show-paren when mc is activated

--- init.d/lisp/el-get/init-multiple-cursors.el
+++ init.d/lisp/el-get/init-multiple-cursors.el
@@ -8,12 +8,16 @@
 (eval-when-compile (require 'org))
 
 (require 'multiple-cursors)
+
 (global-set-key (kbd "C-@") 'mc/mark-all-dwim)
 (global-set-key (kbd "C-c @ e") 'mc/edit-lines)
 (global-set-key (kbd "C-c @ n") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-c @ p") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c @ a") 'mc/mark-all-like-this)
 (global-set-key (kbd "C-c @ SPC") 'set-mark-command)
+
+(add-to-list 'mc/unsupported-minor-modes 'visible-mark-mode)
+(add-to-list 'mc/unsupported-minor-modes 'show-paren-mode)
 
 (custom-set-variables '(mc/always-run-for-all t))
 
