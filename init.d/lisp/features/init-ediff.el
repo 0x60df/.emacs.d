2c931e74383e33e88bcc4127f52f40ca4030927b
0x60df <0x60df@gmail.com>
2017-04-01 01:11:46 +0900

    Modify unit init-feature
    revert ediff-window-setup-fuction to ediff-setup-windows-default

--- init.d/lisp/features/init-ediff.el
+++ init.d/lisp/features/init-ediff.el
@@ -4,4 +4,4 @@
 
 (eval-after-load 'ediff-wind
   '(custom-set-variables
-    '(ediff-window-setup-function 'ediff-setup-windows-plain)))
+    '(ediff-window-setup-function 'ediff-setup-windows-default)))
