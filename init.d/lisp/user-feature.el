13fca6c4a9ebd7abd50e79da8e8c38c6f9fab14f
0x60df <0x60df@gmail.com>
2017-04-02 20:44:38 +0900

    Modify init.el
    move codes about user-feature into unit user-feature

---
+++ init.d/lisp/user-feature.el
@@ -0,0 +1,23 @@
+
+;;;; user-feature.el
+
+
+(mapc (lambda (e) (add-to-list 'load-path e))
+      (reverse
+       (letrec ((filter
+                 (lambda (p l)
+                   (cond ((null l) l)
+                         ((funcall p (car l)) (funcall filter p (cdr l)))
+                         (t (cons (car l) (funcall filter p (cdr l))))))))
+         (apply 'append
+                (mapcar
+                 (lambda (d)
+                   (funcall
+                    filter
+                    (lambda (f) (not (file-directory-p f)))
+                    (mapcar (lambda (f) (expand-file-name (concat d "/" f)))
+                            (remove ".." (directory-files d)))))
+                 '("~/.emacs.d/site-lisp" "~/.emacs.d/lisp"))))))
+
+
+(resolve user-feature)
--- init.el
+++ init.el
@@ -156,6 +156,7 @@
 ;;; packages
 
 (init-package el-get)
+
 (init-package auto-complete)
 (init-package yasnippet)
 (init-package multiple-cursors)
@@ -206,22 +207,7 @@
 
 ;;; user-features
 
-(mapc (lambda (e) (add-to-list 'load-path e))
-      (reverse
-       (letrec ((filter
-                 (lambda (p l)
-                   (cond ((null l) l)
-                         ((funcall p (car l)) (funcall filter p (cdr l)))
-                         (t (cons (car l) (funcall filter p (cdr l))))))))
-         (apply 'append
-                (mapcar
-                 (lambda (d)
-                   (funcall
-                    filter
-                    (lambda (f) (not (file-directory-p f)))
-                    (mapcar (lambda (f) (expand-file-name (concat d "/" f)))
-                            (remove ".." (directory-files d)))))
-                 '("~/.emacs.d/site-lisp" "~/.emacs.d/lisp"))))))
+(init user-feature)
 
 (init-feature ox-gfm)
 (init-feature ox-qmd)
