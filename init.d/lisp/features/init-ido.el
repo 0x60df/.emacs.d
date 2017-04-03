06533b597b655797429cf5989677da02464a5dbd
0x60df <0x60df@gmail.com>
2017-04-03 21:12:47 +0900

    Modify unit init-ido
    Enable minor-mode in init

--- init.d/lisp/features/init-ido.el
+++ init.d/lisp/features/init-ido.el
@@ -9,6 +9,8 @@
 
 (eval-and-compile (require 'ido))
 
+(ido-mode 1)
+
 (eval-after-load 'ido
   '(custom-set-variables '(ido-enable-flex-matching t)
                          '(ido-auto-merge-work-directories-length -1)))
