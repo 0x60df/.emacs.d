7e8c9f442ea7cbb1d7e044ef3ed99639559ebbd1
0x60df <0x60df@gmail.com>
2017-05-05 00:01:33 +0900

    Modify unit init-multi-term
    work with helm

--- init.d/lisp/packages/init-multi-term.el
+++ init.d/lisp/packages/init-multi-term.el
@@ -7,16 +7,20 @@
 
 (global-set-key (kbd "H-t") 'multi-term)
 (eval-after-load 'multi-term
-  '(custom-set-variables
-   `(term-bind-key-alist ',(append term-bind-key-alist
-                                   '(("C-p" . term-send-up)
-                                     ("C-n" . term-send-down)
-                                     ("M-p" . previous-line)
-                                     ("M-n" . next-line)
-                                     ("C-s" . nil)
-                                     ("C-r" . term-send-reverse-search-history)
-                                     ("M-s" . isearch-forward)
-                                     ("M-r" . isearch-backward))))))
+  '(progn
+     (custom-set-variables
+      `(term-unbind-key-list
+        ',(cons "C-q" term-unbind-key-list))) ; work with helm
+     (custom-set-variables
+      `(term-bind-key-alist
+        ',(append term-bind-key-alist
+                  '(("C-p" . term-send-up)
+                    ("C-n" . term-send-down)
+                    ("M-p" . previous-line)
+                    ("M-n" . next-line)
+                    ("C-r" . term-send-reverse-search-history)
+                    ("M-s" . isearch-forward)
+                    ("M-r" . isearch-backward)))))))
 
 
 (resolve init-multi-term)
