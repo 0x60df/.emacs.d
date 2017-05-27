e45fa42c03866574867455f2556727ae0c6a8918
0x60df <0x60df@gmail.com>
2017-05-28 04:02:30 +0900

    Modify unit greeting
    add code for startup message

--- init.d/lisp/greeting.el
+++ init.d/lisp/greeting.el
@@ -8,5 +8,11 @@
  '(inhibit-startup-screen t)
  '(initial-scratch-message nil))
 
+(add-hook
+ 'emacs-startup-hook
+ (lambda ()
+   (message
+    "Welcome to GNU Emacs, one component of the GNU operating system.")))
+
 
 (resolve greeting)
