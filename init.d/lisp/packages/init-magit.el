65060bca8dc437b2c2d32f69107c94b0803ef250
0x60df <0x60df@gmail.com>
2017-06-09 17:25:55 +0900

    Modify unit init-magit
    remove binding magit-checkout.
    magit-status window have command b:branch for checkout

--- init.d/lisp/packages/init-magit.el
+++ init.d/lisp/packages/init-magit.el
@@ -9,7 +9,6 @@
 (premise inst-magit)
 
 (global-set-key "\C-cvm" 'magit-status)
-(global-set-key "\C-cvc" 'magit-checkout)
 (mapc (lambda (hook)
         (add-hook hook (lambda ()
                          (setq show-trailing-whitespace nil))))
