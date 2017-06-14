595a5dfdd430e9f4f46651bba12e73d342a6546a
0x60df <0x60df@gmail.com>
2017-06-15 00:57:34 +0900

    Modify unit init-shifter
    add binding for shift-minor-mode, turn-on-minor-mode, turn-off-minor-mode

--- init.d/lisp/user-features/init-shifter.el
+++ init.d/lisp/user-features/init-shifter.el
@@ -4,6 +4,9 @@
 (premise init)
 
 (global-set-key (kbd "C-c m") 'shifter-shift-major-mode)
+(global-set-key (kbd "C-l m") 'shifter-shift-minor-mode)
+(global-set-key (kbd "s-m e") 'shifter-turn-on-minor-mode)
+(global-set-key (kbd "s-m d") 'shifter-turn-off-minor-mode)
 
 
 (resolve scratch)
