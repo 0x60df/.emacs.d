590008c4227314bcc0d92ac0d5280e42f3b7aac0
0x60df <0x60df@gmail.com>
2017-07-18 01:44:21 +0900

    Modify unit init-yasnippet
    hide menu

--- init.d/lisp/packages/init-yasnippet.el
+++ init.d/lisp/packages/init-yasnippet.el
@@ -18,8 +18,8 @@
      (define-key yas-minor-mode-map (kbd "TAB") nil)
      (define-key yas-minor-mode-map (kbd "C-'") 'yas-expand)
      (setcar (cdr (assq 'yas-minor-mode minor-mode-alist)) " YS")
-     ;; force first match for same keyword
-     '(custom-set-variables '(yas-prompt-functions nil))))
+     (custom-set-variables '(yas-prompt-functions nil)
+                           '(yas-use-menu nil))))
 
 
 (resolve init-yasnippet)
