5ae87307d428acceefdb93e3f29429b3543fb46c
0x60df <0x60df@gmail.com>
2018-08-28 01:25:48 +0900

    Modify unit init-recentf
    set up idle timer for save list

--- init.d/lisp/features/init-recentf.el
+++ init.d/lisp/features/init-recentf.el
@@ -5,7 +5,13 @@
 (premise init)
 
 (eval-after-load 'recentf
-  '(custom-set-variables '(recentf-max-saved-items 1000)))
+  '(progn
+     (defvar recentf-auto-save-timer nil
+       "Timer used to automatically save the recent list.")
+     (custom-set-variables '(recentf-max-saved-items 1000)
+                           '(recentf-auto-cleanup 30))
+     (setq recentf-auto-save-timer
+           (run-with-idle-timer 60 t 'recentf-save-list))))
 
 
 (resolve init-recentf)
