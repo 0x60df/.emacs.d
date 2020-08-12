65d6099c1c8bbd7b935c6273003d20e831ecd769
0x60df <0x60df@gmail.com>
2020-08-13 04:05:47 +0900

    Modify unit vc
    Add codes for abbreviating vc-mode-line-string

--- init.d/lisp/vc.el
+++ init.d/lisp/vc.el
@@ -6,5 +6,20 @@
 
 (custom-set-variables '(vc-follow-symlinks t))
 
+(defun abbreviate-vc-git-mode-line-string (string)
+  "abbreviate vc-git-mode-line-string."
+  (cond ((string-prefix-p "Git" string)
+         (concat "G"
+                 (substring (cond ((string-suffix-p "master" string)
+                                   (concat
+                                    (substring string 0 (- (length string) 6))
+                                    "m/"))
+                                  (t string))
+                            3)))
+        (t string)))
+
+(advice-add 'vc-git-mode-line-string :filter-return
+            'abbreviate-vc-git-mode-line-string)
+
 
 (resolve vc)
