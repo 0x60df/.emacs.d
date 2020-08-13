98353738219fada6d72612613b02616e68de005d
0x60df <0x60df@gmail.com>
2020-08-14 00:59:57 +0900

    Modify user-feature scratch and unit init-scratch
    Add function scratch-shred which kills all scratch buffers.

--- init.d/lisp/user-features/init-scratch.el
+++ init.d/lisp/user-features/init-scratch.el
@@ -5,6 +5,7 @@
 (premise init)
 
 (global-set-key (kbd "C-c b") 'scratch)
+(global-set-key (kbd "C-l b s") 'scratch-shred)
 (define-key scratch-mode-map (kbd "ESC ESC DEL") #'scratch-kill-current-buffer)
 
 
--- lisp/scratch.el
+++ lisp/scratch.el
@@ -8,6 +8,8 @@
   "Generate new buffer instantly"
   :group 'emacs)
 
+(defvar scratch-list '() "Scratch buffer list.")
+
 ;;;###autoload
 (defvar scratch-mode-map (make-sparse-keymap) "Keymap for scratch-mode.")
 
@@ -15,6 +17,7 @@
 (defun scratch-kill-current-buffer ()
   "Kill current buffer"
   (interactive)
+  (setq scratch-list (remove (current-buffer) scratch-list))
   (kill-buffer (current-buffer)))
 
 ;;;###autoload
@@ -34,6 +37,13 @@ Reservation is restricted on current buffer."
             nil t)
   (remove-hook 'after-change-major-mode-hook #'scratch-mode-buffer-sticky))
 
+(defun scratch-shred ()
+  "Kill all alive scratch buffer."
+  (interactive)
+  (when(yes-or-no-p "Kill all scratch buffers? ")
+      (mapc #'kill-buffer scratch-list)
+      (setq scratch-list '())))
+
 ;;;###autoload
 (defun scratch ()
   "Generate new buffer instantly."
@@ -44,7 +54,8 @@ Reservation is restricted on current buffer."
     (switch-to-buffer buffer)
     (if (not (with-local-quit (shifter-shift-major-mode) t))
         (kill-buffer buffer)
-      (scratch-mode-buffer-sticky))))
+      (scratch-mode-buffer-sticky)
+      (add-to-list 'scratch-list buffer))))
 
 (provide 'scratch)
 
