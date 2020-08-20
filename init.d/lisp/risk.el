663933d8797aba77f158cc737d3ebe73511d6c3e
0x60df <0x60df@gmail.com>
2020-08-20 21:55:12 +0900

    Modify unit risk
    Change binding mechanism.
    minibuffer-local-map has been used.
    Now, minor-mode is used instead of local-map.

--- init.d/lisp/risk.el
+++ init.d/lisp/risk.el
@@ -10,32 +10,29 @@
 
 ;;; yes-or-no-p
 
-(defun exit-yes-or-no-p-with-yes-insertion ()
-  (interactive)
-  (insert "yes")
-  (exit-minibuffer))
+(define-minor-mode risky-yes-or-no-p-transient-mode
+  "Minor mode holding risky keymap for `yes-or-no-p'."
+  :global t
+  :keymap `(("\C-j" . (lambda ()
+                        (interactive)
+                        (when (minibufferp (current-buffer))
+                          (insert "yes")
+                          (exit-minibuffer))))))
 
-(defun enable-temporal-key-bind-on-yes-or-no-p (fun &rest args)
-  (let* ((key-seq (kbd "C-j"))
-         (bound-func  (lookup-key minibuffer-local-map key-seq)))
-    (unwind-protect
-        (progn
-          (define-key minibuffer-local-map key-seq
-            #'exit-yes-or-no-p-with-yes-insertion)
-          (apply fun args))
-      (define-key minibuffer-local-map key-seq
-        (if (equal bound-func #'exit-yes-or-no-p-with-yes-insertion)
-            'exit-minibuffer
-          bound-func)))))
+(defun with-risky-yes-or-no-p-transient-mode (fun &rest args)
+  (unwind-protect
+      (progn
+        (risky-yes-or-no-p-transient-mode 1)
+        (apply fun args))
+    (risky-yes-or-no-p-transient-mode -1)))
 
 (define-minor-mode risky-yes-or-no-p-mode
-  "Allow yes return by C-return, and no return by M-return."
+  "Allow answering yes by C-j on `yes-or-no-p'."
   :global t
   :lighter " R"
   (if risky-yes-or-no-p-mode
-      (advice-add 'yes-or-no-p :around
-                  #'enable-temporal-key-bind-on-yes-or-no-p)
-    (advice-remove 'yes-or-no-p #'enable-temporal-key-bind-on-yes-or-no-p)))
+      (advice-add 'yes-or-no-p :around #'with-risky-yes-or-no-p-transient-mode)
+    (advice-remove 'yes-or-no-p #'with-risky-yes-or-no-p-transient-mode)))
 
 (global-set-key (kbd "C-c r y") #'risky-yes-or-no-p-mode)
 
