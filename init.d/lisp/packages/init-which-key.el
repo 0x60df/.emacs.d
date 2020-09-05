e99db234b143730c5dd2c4a7d4dfa19a6c8df796
0x60df <0x60df@gmail.com>
2020-09-06 02:45:00 +0900

    Modify unit init-which-key
    Add settings for paging features.

--- init.d/lisp/packages/init-which-key.el
+++ init.d/lisp/packages/init-which-key.el
@@ -5,9 +5,19 @@
 (premise init)
 (premise inst-which-key)
 
+(eval-when-compile (require 'which-key))
+
 (eval-after-load 'which-key
-  '(custom-set-variables '(which-key-idle-delay 0.4)
-                         '(which-key-lighter "")))
+  '(progn
+     (custom-set-variables '(which-key-idle-delay 0.4)
+                           '(which-key-lighter "")
+                           '(which-key-paging-key "C-?"))
+     (mapc
+      (lambda (s)
+        (define-key which-key-mode-map
+          (kbd (concat s " " which-key-paging-key))
+          'which-key-C-h-dispatch))
+      '("C-x" "C-c h" "C-q"))))
 
 (which-key-mode 1)
 
