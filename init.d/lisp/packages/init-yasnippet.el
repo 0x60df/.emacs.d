7ec2c6e537a89109ec1b6ad26b7d098d27e95788
0x60df <0x60df@gmail.com>
2020-09-14 22:54:08 +0900

    Modify unit init-yasnippet
    Turn off global-mode and add-hook for prog-mode-hook.

--- init.d/lisp/packages/init-yasnippet.el
+++ init.d/lisp/packages/init-yasnippet.el
@@ -10,7 +10,8 @@
 
 (require 'yasnippet)
 
-(yas-global-mode 1)
+(yas-reload-all)
+(add-hook 'prog-mode-hook #'yas-minor-mode)
 
 (define-key yas-minor-mode-map (kbd "<tab>") nil)
 (define-key yas-minor-mode-map (kbd "TAB") nil)
@@ -18,16 +19,6 @@
 (setcar (cdr (assq 'yas-minor-mode minor-mode-alist)) " YS")
 (custom-set-variables '(yas-prompt-functions nil)
                       '(yas-use-menu nil))
-
-(add-hook 'after-init-hook
-          (lambda ()
-            (with-current-buffer "*Messages*"
-              (yas-minor-mode -1))))
-
-(defun disable-yas-if-no-snippets ()
-  (when (and yas-minor-mode (null (yas--get-snippet-tables)))
-    (yas-minor-mode -1)))
-(add-hook 'yas-minor-mode-hook #'disable-yas-if-no-snippets)
 
 
 (resolve init-yasnippet)
