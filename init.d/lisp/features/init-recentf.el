565e63244a97dcc7b16c861ac5afe37644361b57
0x60df <0x60df@gmail.com>
2020-09-24 00:13:51 +0900

    Modify unit init-recentf
    Set long time to auto cleanup and save list.
    Move custom-set-variables to the outside of with-eval-after-load.

--- init.d/lisp/features/init-recentf.el
+++ init.d/lisp/features/init-recentf.el
@@ -4,14 +4,14 @@
 
 (premise init)
 
-(eval-after-load 'recentf
-  '(progn
-     (defvar recentf-auto-save-timer nil
-       "Timer used to automatically save the recent list.")
-     (custom-set-variables '(recentf-max-saved-items 1000)
-                           '(recentf-auto-cleanup 30))
-     (setq recentf-auto-save-timer
-           (run-with-idle-timer 60 t 'recentf-save-list))))
+(custom-set-variables '(recentf-max-saved-items 1000)
+                      '(recentf-auto-cleanup (* 60 60)))
+
+(with-eval-after-load 'recentf
+  (defvar recentf-auto-save-timer nil
+    "Timer used to automatically save the recent list.")
+  (setq recentf-auto-save-timer
+        (run-with-idle-timer (* 60 61) t 'recentf-save-list)))
 
 
 (resolve init-recentf)
