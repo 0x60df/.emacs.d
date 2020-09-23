88f7e0c8738d67111917dbd72de5df96db885a61
0x60df <0x60df@gmail.com>
2020-09-24 00:16:01 +0900

    Modify unit init-fmmm
    Set long time to save cache.
    Add document string for timer variable.

--- init.d/lisp/user-features/init-fmmm.el
+++ init.d/lisp/user-features/init-fmmm.el
@@ -8,13 +8,14 @@
                         '(shell-script-mode ruby-mode)))
 
 (with-eval-after-load 'fmmm
-  (defvar fmmm-cache-auto-save-timer)
+  (defvar fmmm-cache-auto-save-timer nil
+    "Timer used to automatically save the fmmm cache.")
   (setq fmmm-cache-auto-save-timer
         (run-with-idle-timer
-         80 t (lambda ()
-                (fmmm-update-major-mode-on-autoload-list)
-                (fmmm-update-minor-mode-on-autoload-list)
-                (fmmm-save-cache))))
+         (* 60 62) t (lambda ()
+                       (fmmm-update-major-mode-on-autoload-list)
+                       (fmmm-update-minor-mode-on-autoload-list)
+                       (fmmm-save-cache))))
   (fmmm-autoload-collector-mode 1))
 
 
