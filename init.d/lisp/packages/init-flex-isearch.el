2c1272f95627a9f005e0ac693bab6bcb9a6edf84
0x60df <0x60df@gmail.com>
2020-09-29 02:41:04 +0900

    Modify unit init-flex-isearch
    Add key bindings.
    Modify message prefix.

--- init.d/lisp/packages/init-flex-isearch.el
+++ init.d/lisp/packages/init-flex-isearch.el
@@ -7,5 +7,12 @@
 
 (global-flex-isearch-mode 1)
 
+(global-set-key (kbd "C-S-s") 'flex-isearch-forward)
+(global-set-key (kbd "C-S-r") 'flex-isearch-backward)
+
+(custom-set-variables
+ '(flex-isearch-message-prefix
+   (propertize "[FLEX] " 'face '(font-lock-constant-face))))
+
 
 (resolve init-flex-isearch)
