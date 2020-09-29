f0143f0e072ff129382c894bf40ae2ae5b4f781b
0x60df <0x60df@gmail.com>
2020-09-30 00:53:42 +0900

    Modify unit custom
    Add documentation string for defgroup user.
    Remove utility function which is rarely used.

--- init.d/lisp/custom.el
+++ init.d/lisp/custom.el
@@ -4,15 +4,11 @@
 
 (premise init)
 
-(defgroup user nil "" :group 'emacs)
+(defgroup user nil "Group for user customization" :group 'emacs)
 
-(custom-set-variables '(custom-file (concat user-emacs-directory "custom.el")))
+(custom-set-variables
+ '(custom-file (concat user-emacs-directory "custom.el")))
 (add-hook 'after-init-hook (lambda () (load custom-file 'noerror)))
-
-(defun custom-clean-settings ()
-  (interactive)
-  (if (and custom-file (file-exists-p custom-file))
-      (delete-file custom-file)))
 
 
 (resolve custom)
