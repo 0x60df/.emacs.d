6ffbaef9e6e715a440eeebdf206872067138ec21
0x60df <0x60df@gmail.com>
2020-09-30 23:21:00 +0900

    Modify unit paren
    Remove meaningless eval-after-load.

--- init.d/lisp/paren.el
+++ init.d/lisp/paren.el
@@ -7,8 +7,7 @@
 (eval-when-compile (require 'paren))
 
 (show-paren-mode 1)
-(eval-after-load 'paren
-  '(custom-set-variables '(show-paren-style 'mixed)))
+(custom-set-variables '(show-paren-style 'mixed))
 
 
 (resolve paren)
