0e8bf6da890a6748c5d5f9d7249ded7d1c51dc2e
0x60df <0x60df@gmail.com>
2020-09-30 23:39:30 +0900

    Style unit paren

--- init.d/lisp/paren.el
+++ init.d/lisp/paren.el
@@ -7,7 +7,8 @@
 (eval-when-compile (require 'paren))
 
 (show-paren-mode 1)
-(custom-set-variables '(show-paren-style 'mixed))
+(custom-set-variables
+ '(show-paren-style 'mixed))
 
 
 (resolve paren)
