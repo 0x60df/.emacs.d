91e6cb5421c1e66236c8fa179e749bf534e10e13
0x60df <0x60df@gmail.com>
2020-10-02 01:17:05 +0900

    Modify unit insurance
    Change the number of kept vertions.

--- init.d/lisp/insurance.el
+++ init.d/lisp/insurance.el
@@ -8,8 +8,8 @@
  '(backup-directory-alist `((".*" . ,(concat user-emacs-directory "backup"))))
  '(backup-by-copying t)
  '(version-control t)
- '(kept-new-versions 5)
- '(kept-old-versions 1)
+ '(kept-new-versions 4)
+ '(kept-old-versions 2)
  '(delete-old-versions t)
  '(delete-auto-save-files t))
 
