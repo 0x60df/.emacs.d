59a710fc5d595a452329ba8fdd9212d169393fb0
0x60df <0x60df@gmail.com>
2020-10-05 00:49:34 +0900

    Modify unit vc
    Add premise custom.
    Refactor and style codes.

--- init.d/lisp/vc.el
+++ init.d/lisp/vc.el
@@ -3,11 +3,13 @@
 
 
 (premise init)
+(premise custom)
 
-(custom-set-variables '(vc-follow-symlinks t))
+(custom-set-variables
+ '(vc-follow-symlinks t))
 
 (defun abbreviate-vc-git-mode-line-string (string)
-  "abbreviate vc-git-mode-line-string."
+  "Abbreviate vc-git-mode-line-string."
   (cond ((string-prefix-p "Git" string)
          (concat "G"
                  (substring (cond ((string-suffix-p "master" string)
@@ -18,8 +20,8 @@
                             3)))
         (t string)))
 
-(advice-add 'vc-git-mode-line-string :filter-return
-            'abbreviate-vc-git-mode-line-string)
+(advice-add 'vc-git-mode-line-string
+            :filter-return #'abbreviate-vc-git-mode-line-string)
 
 
 (resolve vc)
