d0be6a901502c685cd676cb868830430b4de39f8
0x60df <0x60df@gmail.com>
2020-10-04 23:41:30 +0900

    Modify unit risk
    Add function which has been bound in
    risky-yes-or-no-p-transient-mode-map as anonymous function.
    Modify documentation strings.
    Style codes.

--- init.d/lisp/risk.el
+++ init.d/lisp/risk.el
@@ -2,9 +2,6 @@
 ;;;; risk.el
 
 
-
-;;; base
-
 (premise init)
 
 
@@ -16,14 +13,17 @@
   "Face for prefix of risky-yes-or-no-p prompt."
   :group 'user)
 
+(defun risky-yes-or-no-p-yes ()
+  "Answer yes in minibuffer."
+  (interactive)
+  (when (minibufferp (current-buffer))
+    (insert "yes")
+    (exit-minibuffer)))
+
 (define-minor-mode risky-yes-or-no-p-transient-mode
-  "Minor mode holding risky keymap for `yes-or-no-p'."
+  "Minor mode which is transiently turned on during `yes-or-no-p'."
   :global t
-  :keymap `(("\C-j" . (lambda ()
-                        (interactive)
-                        (when (minibufferp (current-buffer))
-                          (insert "yes")
-                          (exit-minibuffer))))))
+  :keymap `(("\C-j" . ,#'risky-yes-or-no-p-yes)))
 
 (defun with-risky-yes-or-no-p-transient-mode (fun arg)
   (unwind-protect
@@ -35,7 +35,10 @@
     (risky-yes-or-no-p-transient-mode -1)))
 
 (define-minor-mode risky-yes-or-no-p-mode
-  "Allow answering yes by C-j on `yes-or-no-p'."
+  "Enable answering yes by one key sequence.
+\\<risky-yes-or-no-p-transient-mode-map>
+\\[risky-yes-or-no-p-yes] which is key binding for `risky-yes-or-no-p-yes'
+realise it as `insert' yes and `exit-minibuffer'."
   :global t
   (if risky-yes-or-no-p-mode
       (advice-add 'yes-or-no-p :around #'with-risky-yes-or-no-p-transient-mode)
@@ -43,5 +46,6 @@
 
 (global-set-key (kbd "C-c r y") #'risky-yes-or-no-p-mode)
 (risky-yes-or-no-p-mode 1)
+
 
 (resolve risk)
