61d4f9f915161cdec2aa563b340867e020bc7a8b
0x60df <0x60df@gmail.com>
2020-10-05 01:30:35 +0900

    Modify unit package
    Use custom-set-variables in spite of add-to-list.
    Remove require package, which will be autoloaded by
    package-initialize.

--- init.d/lisp/package.el
+++ init.d/lisp/package.el
@@ -3,10 +3,15 @@
 
 
 (premise init)
+(premise custom)
 
-(require 'package)
+(custom-set-variables
+ '(package-archives
+   (let ((package-archive '("melpa" . "https://melpa.org/packages/")))
+     (if (member package-archive package-archives)
+         package-archives
+       (cons package-archive package-archives)))))
 
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 
