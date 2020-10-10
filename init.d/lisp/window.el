301e29f79f78d4e6004daf08bf5b1eab007bcedb
0x60df <0x60df@gmail.com>
2020-10-11 01:10:24 +0900

    Modify unit window
    Refine handling prefix argument.
    Add default factor of deformation of a window.
    Style codes as changing variable name.
    Drop support for changing factor value during manipulating, which
    actually does not used.
    Change manipulating command matching scheme as using key-description.
    
    Add support for key input which is not manipulating command.
    If it is self-insert-command, quit manipulate-window.
    Otherwise, call-interactively key-binding of input and quit.
    
    Add documentation string.

--- init.d/lisp/window.el
+++ init.d/lisp/window.el
@@ -3,81 +3,90 @@
 
 
 (premise init)
+(premise custom)
 
 (defun other-window-reverse (arg)
+  "Other window by reverse order."
   (interactive "p")
   (other-window (- arg)))
 
-(defun split-window-below-or-right (arg)
+(defun split-window-below-or-right (&optional arg)
+  "Split window below or right according to a prefix argument."
   (interactive "P")
   (if arg
       (select-window (split-window-right))
     (select-window (split-window-below))))
 
-(defun manipulate-window (arg)
+(defcustom manipulate-window-default-factor 2
+  "Defult factor of deformation for manipulating window."
+  :type 'number
+  :group 'user)
+
+(defun manipulate-window (&optional arg)
+  "Manipulate window size interactively."
   (interactive "P")
   (let* ((initial-width (window-width))
          (initial-height (window-height))
          (initial-right-edge (nth 2 (window-pixel-edges)))
          (initial-bottom-edge (nth 3 (window-pixel-edges)))
-         (ex (if (= initial-right-edge (frame-pixel-width)) -1 1))
-         (ey (if (= initial-bottom-edge
+         (direction-x (if (= initial-right-edge (frame-pixel-width))
+                 -1
+               1))
+         (direction-y (if (= initial-bottom-edge
                     (- (frame-pixel-height)
-                       (window-pixel-height (minibuffer-window)))) -1 1))
-         (r (or arg 1))
-         l
-         c)
+                       (window-pixel-height (minibuffer-window))))
+                 -1
+               1))
+         (factor (if arg
+                     (prefix-numeric-value arg)
+                   manipulate-window-default-factor)))
     (catch 'quit
       (while t
-        (let ((left-edge (nth 0 (window-edges)))
-              (top-edge (nth 1 (window-edges)))
-              (r (if (null l)
-                     r
-                   (let ((digits (remq ?- (reverse l))))
-                     (string-to-number (concat
-                                     (make-string
-                                      (% (- (length l) (length digits)) 2) ?-)
-                                     (or digits "1")))))))
-          (setq c (read-char (format
-                              (concat "position[%02d,%02d]" " "
-                                      "size[%02dx%02d]" " "
-                                      "frame[%02dx%02d]" " "
-                                      "step[%02d]")
-                              left-edge top-edge
-                              (window-width) (window-height)
-                              (frame-width) (frame-height)
-                              r)))
-          (cond ((= c ?f)
-                 (enlarge-window-horizontally (* r ex)))
-                ((= c ?b)
-                 (shrink-window-horizontally (* r ex)))
-                ((= c ?n)
-                 (enlarge-window (* r ey)))
-                ((= c ?p)
-                 (shrink-window (* r ey)))
-                ((= c ?a)
-                 (shrink-window-horizontally (* (frame-width) ex)))
-                ((= c ?e)
-                 (enlarge-window-horizontally (* (frame-width) ex)))
-                ((= c 134217830)        ;M-f
-                 (enlarge-window-horizontally (* initial-width ex)))
-                ((= c 134217826)        ;M-b
-                 (shrink-window-horizontally (* initial-width ex)))
-                ((= c 134217788)        ;M-<
-                 (shrink-window (* (frame-height) ey)))
-                ((= c 134217790)        ;M->
-                 (enlarge-window (* (frame-height) ey)))
-                ((= c ?v)
-                 (enlarge-window (* initial-height ey)))
-                ((= c 134217846)        ;M-v
-                 (shrink-window (* initial-height ey)))
-                ((or (= c ?-) (and (<= ?0 c) (<= c ?9)))
-                 (setq l (cons c l)))
-                ((= c ?g)
-                 (setq l '()))
-                ((= c ?q)
-                 (message "quit")
-                 (throw 'quit t))))))))
+        (let* ((left-edge (nth 0 (window-edges)))
+               (top-edge (nth 1 (window-edges)))
+               (key-sequence (read-key-sequence-vector
+                              (format
+                               (concat "position[%02d,%02d]" " "
+                                       "size[%02dx%02d]" " "
+                                       "frame[%02dx%02d]" " "
+                                       "step[%02d]")
+                               left-edge top-edge
+                               (window-width) (window-height)
+                               (frame-width) (frame-height)
+                               factor)))
+               (key-description (key-description key-sequence))
+               (key-binding (key-binding key-sequence)))
+          (cond ((equal key-description "f")
+                 (enlarge-window-horizontally (* factor direction-x)))
+                ((equal key-description "b")
+                 (shrink-window-horizontally (* factor direction-x)))
+                ((equal key-description "n")
+                 (enlarge-window (* factor direction-y)))
+                ((equal key-description "p")
+                 (shrink-window (* factor direction-y)))
+                ((equal key-description "a")
+                 (shrink-window-horizontally (* (frame-width) direction-x)))
+                ((equal key-description "e")
+                 (enlarge-window-horizontally (* (frame-width) direction-x)))
+                ((equal key-description "M-f")
+                 (enlarge-window-horizontally (* initial-width direction-x)))
+                ((equal key-description "M-b")
+                 (shrink-window-horizontally (* initial-width direction-x)))
+                ((equal key-description "<")
+                 (shrink-window (* (frame-height) direction-y)))
+                ((equal key-description ">")
+                 (enlarge-window (* (frame-height) direction-y)))
+                ((equal key-description "v")
+                 (enlarge-window (* initial-height direction-y)))
+                ((equal key-description "M-v")
+                 (shrink-window (* initial-height direction-y)))
+                ((equal key-description "q")
+                 (throw 'quit t))
+                ((and (not (eq key-binding 'self-insert-command))
+                      (commandp key-binding))
+                 (call-interactively key-binding)
+                 (throw 'quit t))
+                (t (throw 'quit t))))))))
 
 
 (resolve window)
