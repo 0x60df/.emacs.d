e78f178ddbaee51262b84227acd1e85e4d9c0d84
0x60df <0x60df@gmail.com>
2020-10-20 22:05:14 +0900

    Modify unit init-eww
    Add premise bindings and whitespace.
    Use custom-set-variables instead of setq.
    Wrap add-hook by with-eval-after-load.
    Style codes.

--- init.d/lisp/features/init-eww.el
+++ init.d/lisp/features/init-eww.el
@@ -3,14 +3,17 @@
 
 
 (premise init)
+(premise custom)
+(premise bindings)
+(premise whitespace)
 
-(eval-when-compile (require 'eww))
+(custom-set-variables
+ '(eww-search-prefix "https://www.google.co.jp/search?q="))
 
-(setq eww-search-prefix "https://www.google.co.jp/search?q=")
+(global-set-key (kbd "C-c w") #'eww)
 
-(global-set-key "\C-cw" 'eww)
-
-(add-hook 'eww-mode-hook (lambda () (setq show-trailing-whitespace nil)))
+(with-eval-after-load 'eww
+  (add-hook 'eww-mode-hook (lambda () (setq show-trailing-whitespace nil))))
 
 
 (resolve init-eww)
