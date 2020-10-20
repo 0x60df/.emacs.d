ff20133c47d7db4f4c4750420d7fac113090ec2e
0x60df <0x60df@gmail.com>
2020-10-20 22:04:00 +0900

    Modify unit init-eshell
    Add premise bindings and whitespace.
    Wrap add-hook by with-eval-after-load.
    Style codes.

--- init.d/lisp/features/init-eshell.el
+++ init.d/lisp/features/init-eshell.el
@@ -3,12 +3,17 @@
 
 
 (premise init)
+(premise bindings)
+(premise whitespace)
 
-(defun eshell/ediff (file-A file-B) (ediff file-A file-B))
+(defun eshell/ediff (file-A file-B)
+  "ediff for eshell command."
+  (ediff file-A file-B))
 
-(global-set-key "\C-ct" 'eshell)
+(global-set-key (kbd "C-c t") #'eshell)
 
-(add-hook 'eshell-mode-hook (lambda () (setq show-trailing-whitespace nil)))
+(with-eval-after-load 'esh-mode
+  (add-hook 'eshell-mode-hook (lambda () (setq show-trailing-whitespace nil))))
 
 
 (resolve init-eshell)
