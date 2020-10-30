182f8382f0f81cd5863cebc68e7504f6c4aa646f
0x60df <0x60df@gmail.com>
2020-10-30 23:42:00 +0900

    Modify unit init-shifter
    Add premise user-feature.
    Style codes.

--- init.d/lisp/user-features/init-shifter.el
+++ init.d/lisp/user-features/init-shifter.el
@@ -3,7 +3,10 @@
 
 
 (premise init)
+(premise user-feature)
 (premise bindings)
+
+(declare-function shifter-save-hist "shifter")
 
 (overriding-set-key (kbd "C-c m") #'shifter-shift-major-mode)
 (overriding-set-key (kbd "C-l m") #'shifter-shift-minor-mode)
@@ -12,7 +15,7 @@
   (defvar shifter-hist-auto-save-timer nil
     "Timer used to automatically save the shifter hist.")
   (setq shifter-hist-auto-save-timer
-        (run-with-idle-timer (* 60 63) t 'shifter-save-hist)))
+        (run-with-idle-timer (* 60 63) t #'shifter-save-hist)))
 
 
 (resolve shifter)
