b271999096d1108a7f34263770950772e8e7cbee
0x60df <0x60df@gmail.com>
2020-10-30 23:34:57 +0900

    Style init-elisp-mode

--- init.d/lisp/features/init-elisp-mode.el
+++ init.d/lisp/features/init-elisp-mode.el
@@ -4,10 +4,9 @@
 
 (premise init)
 
-(eval-after-load 'elisp-mode
-  '(progn
-    (add-hook 'lisp-interaction-mode-hook (lambda () (setq mode-name "Lisp I")))
-    (add-hook 'emacs-lisp-mode-hook (lambda () (setq mode-name "E-Lisp")))))
+(with-eval-after-load 'elisp-mode
+  (add-hook 'lisp-interaction-mode-hook (lambda () (setq mode-name "Lisp I")))
+  (add-hook 'emacs-lisp-mode-hook (lambda () (setq mode-name "E-Lisp"))))
 
 
 (resolve init-elisp-mode)
