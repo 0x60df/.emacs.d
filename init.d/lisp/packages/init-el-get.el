770c59203ca2ce5edd9274286c99f31cd4730e10
0x60df <0x60df@gmail.com>
2020-10-31 12:53:56 +0900

    Modify unit init-el-get
    Add premise custom.
    Move custom-set-variables to out of eval-after-load.

--- init.d/lisp/packages/init-el-get.el
+++ init.d/lisp/packages/init-el-get.el
@@ -3,12 +3,12 @@
 
 
 (premise init)
+(premise custom)
 (premise inst-el-get)
 
-(eval-after-load 'el-get-recipes
-  '(custom-set-variables
-    '(el-get-user-package-directory
-      (concat user-emacs-directory "el-get-user/init-files"))))
+(custom-set-variables
+ '(el-get-user-package-directory
+   (concat user-emacs-directory "el-get-user/init-files")))
 
 
 (resolve init-el-get)
