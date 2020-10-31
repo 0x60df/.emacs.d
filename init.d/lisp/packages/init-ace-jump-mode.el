ada10e57c3ee144144cb7ce9615bb37512745318
0x60df <0x60df@gmail.com>
2020-10-31 17:16:01 +0900

    Modify unit init-ace-jump-mode
    Bind keys by overriding-set-key.
    Name each function bound to A-.

--- init.d/lisp/packages/init-ace-jump-mode.el
+++ init.d/lisp/packages/init-ace-jump-mode.el
@@ -3,26 +3,34 @@
 
 
 (premise init)
+(premise bindings)
 (premise inst-ace-jump-mode)
 
-(global-set-key (kbd "M-g M-j") 'ace-jump-mode)
-(global-set-key (kbd "H-g") 'ace-jump-mode)
+(overriding-set-key (kbd "M-g M-j") #'ace-jump-mode)
+(overriding-set-key (kbd "H-g") #'ace-jump-mode)
 (mapc (lambda (c)
-        (eval-after-load 'iso-transl
-          `(define-key key-translation-map (vector (logior ,c ?\A-\^@)) nil))
-        (global-set-key
-         (kbd (concat "A-" (char-to-string c)))
-         `(lambda ()
-            (interactive)
-            (funcall 'ace-jump-char-mode ,c)))
-        (global-set-key
-         (kbd (concat "A-M-" (char-to-string c)))
-         `(lambda ()
-            (interactive)
-            (funcall 'ace-jump-word-mode ,c))))
+        (let ((s (char-to-string c)))
+          (eval-after-load 'iso-transl
+            `(define-key key-translation-map (vector (logior ,c ?\A-\^@)) nil))
+
+          (let ((jump-char-func (intern (concat "ace-jump-char-" s))))
+            (fset jump-char-func
+                  `(lambda ()
+                     ,(format "Ace jump char to `%s'." s)
+                     (interactive)
+                     (funcall-interactively 'ace-jump-char-mode ,c)))
+            (overriding-set-key (kbd (concat "A-" s)) jump-char-func))
+
+          (let ((jump-word-func (intern (concat "ace-jump-word-" s))))
+            (fset jump-word-func
+                  `(lambda ()
+                     ,(format "Ace jump word to `%s'." s)
+                     (interactive)
+                     (funcall-interactively 'ace-jump-word-mode ,c)))
+            (overriding-set-key (kbd (concat "A-C-" s)) jump-word-func))))
       (vconcat "!\"#$%&'()*+,-./0123456789:;<=>?@"
                "[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"))
-(global-set-key (read-kbd-macro "A-<return>") #'ace-jump-line-mode)
+(overriding-set-key (read-kbd-macro "A-<return>") #'ace-jump-line-mode)
 
 
 (resolve init-ace-jump-mode)
