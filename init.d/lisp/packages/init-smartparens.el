5ad4dd5a152dce8a94b11740be278c2e330e7f65
0x60df <0x60df@gmail.com>
2020-10-31 21:28:55 +0900

    Modify unit init-smartparens
    Use overriding-set-key instead of global-set-key.
    Wrap loading codes by add-hook 'emacs-startup-hook.
    Add pair definition for emacs-lisp-mode and lisp-interaction-mode.

--- init.d/lisp/packages/init-smartparens.el
+++ init.d/lisp/packages/init-smartparens.el
@@ -2,27 +2,36 @@
 ;;;; smartparens.el
 
 
-
-;;; base
-
 (premise init)
+(premise custom)
+(premise bindings)
 (premise inst-smartparens)
 
-(require 'smartparens)
+(declare-function sp-splice-sexp "smartparens")
+(declare-function sp-rewrap-sexp "smartparens")
+(declare-function sp-local-pair "smartparens")
 
-(smartparens-global-mode)
-(custom-set-variables '(sp-highlight-pair-overlay nil)
-                      '(sp-highlight-wrap-overlay nil)
-                      '(sp-highlight-wrap-tag-overlay nil))
-(sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
-(sp-local-pair 'lisp-interaction-mode "'" nil :actions nil)
-(sp-local-pair 'emacs-lisp-mode "`" nil :actions nil)
-(sp-local-pair 'lisp-interaction-mode "`" nil :actions nil)
+(custom-set-variables
+ '(sp-highlight-pair-overlay nil)
+ '(sp-highlight-wrap-overlay nil)
+ '(sp-highlight-wrap-tag-overlay nil))
 
-;;; bindings
+(overriding-set-key (kbd "C-(") #'sp-splice-sexp)
+(overriding-set-key (kbd "C-)") #'sp-rewrap-sexp)
 
-(global-set-key (kbd "C-(") 'sp-splice-sexp)
-(global-set-key (kbd "C-)") 'sp-rewrap-sexp)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (require 'smartparens)
+
+            (mapc (lambda (mode)
+                    (sp-local-pair mode "'" nil :actions nil)
+                    (sp-local-pair mode "`" "'"
+                                   :when '(sp-in-docstring-p
+                                           sp-in-string-p
+                                           sp-in-comment-p)))
+                  '(emacs-lisp-mode lisp-interaction-mode))
+
+            (smartparens-global-mode)))
 
 
 (resolve init-smartparens)
