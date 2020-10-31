4cdfccd372d7ce1d3f0e71c5d573f47e817faa70
0x60df <0x60df@gmail.com>
2020-10-31 14:59:07 +0900

    Modify unit init-loophole
    Add premise statements
    Bind keys by overriding-set-key.

--- init.d/lisp/packages/init-loophole.el
+++ init.d/lisp/packages/init-loophole.el
@@ -2,36 +2,47 @@
 ;;;; init-loophole.el
 
 
-
-;;; base
+(premise init)
+(premise custom)
+(premise bindings)
+(premise inst-loophole)
 
 (require 'loophole)
 
+(overriding-set-key (kbd "C-]") #'loophole-set-key)
+(overriding-set-key (kbd "C-c ] e") #'loophole-enable-map)
+(overriding-set-key (kbd "C-c ] ]") #'loophole-mode)
 
-(global-set-key (kbd "C-]") 'loophole-set-key)
-(global-set-key (kbd "C-c ] e") 'loophole-enable-map)
-(global-set-key (kbd "C-c ] ]") 'loophole-mode)
-(setcdr loophole-mode-map nil)
-(define-key loophole-mode-map (kbd "C-c \\") #'loophole-mode)
-(define-key loophole-mode-map (kbd "C-c ,") #'loophole-disable-last-map)
-(define-key loophole-mode-map (kbd "C-c .") #'loophole-quit)
-(define-key loophole-mode-map (kbd "C-c /") #'loophole-stop-edit)
-(define-key loophole-mode-map (kbd "C-c ?") #'loophole-start-edit)
-(define-key loophole-mode-map (kbd "C-c ] s") #'loophole-set-key)
-(define-key loophole-mode-map (kbd "C-c ] u") #'loophole-unset-key)
-(define-key loophole-mode-map (kbd "C-c ] d") #'loophole-disable-map)
-(define-key loophole-mode-map (kbd "C-c ] D") #'loophole-disable-all-maps)
-(define-key loophole-mode-map (kbd "C-c ] (") #'loophole-start-kmacro)
-(define-key loophole-mode-map (kbd "C-c ] )") #'loophole-end-kmacro)
-(define-key loophole-mode-map (kbd "C-c ] k s") #'loophole-start-kmacro)
-(define-key loophole-mode-map (kbd "C-c ] k e") #'loophole-end-kmacro)
-(define-key loophole-mode-map (kbd "C-c ] k a") #'loophole-abort-kmacro)
-(define-key loophole-mode-map (kbd "C-c ] k b") #'loophole-bind-last-kmacro)
-(define-key loophole-mode-map (kbd "C-c ] b e") #'loophole-bind-entry)
-(define-key loophole-mode-map (kbd "C-c ] b c") #'loophole-bind-command)
-(define-key loophole-mode-map (kbd "C-c ] b k") #'loophole-bind-kmacro)
+(with-eval-after-load 'loophole
+  (setcdr loophole-mode-map nil)
 
-(loophole-mode-set-lighter-format 'tag)
+  (defvar overriding-loophole-mode-map
+    (let ((map (make-sparse-keymap)))
+      (define-key map (kbd "C-c \\") #'loophole-mode)
+      (define-key map (kbd "C-c ,") #'loophole-disable-last-map)
+      (define-key map (kbd "C-c .") #'loophole-quit)
+      (define-key map (kbd "C-c /") #'loophole-stop-edit)
+      (define-key map (kbd "C-c ?") #'loophole-start-edit)
+      (define-key map (kbd "C-c ] s") #'loophole-set-key)
+      (define-key map (kbd "C-c ] u") #'loophole-unset-key)
+      (define-key map (kbd "C-c ] d") #'loophole-disable-map)
+      (define-key map (kbd "C-c ] D") #'loophole-disable-all-maps)
+      (define-key map (kbd "C-c ] (") #'loophole-start-kmacro)
+      (define-key map (kbd "C-c ] )") #'loophole-end-kmacro)
+      (define-key map (kbd "C-c ] k s") #'loophole-start-kmacro)
+      (define-key map (kbd "C-c ] k e") #'loophole-end-kmacro)
+      (define-key map (kbd "C-c ] k a") #'loophole-abort-kmacro)
+      (define-key map (kbd "C-c ] k b") #'loophole-bind-last-kmacro)
+      (define-key map (kbd "C-c ] b e") #'loophole-bind-entry)
+      (define-key map (kbd "C-c ] b c") #'loophole-bind-command)
+      (define-key map (kbd "C-c ] b k") #'loophole-bind-kmacro)
+      map)
+    "Keymap for ‘loophole-mode’ which overrides global overriding maps.")
+
+  (push `(loophole-mode . ,overriding-loophole-mode-map)
+        overriding-reserved-key-map-alist)
+
+  (loophole-mode-set-lighter-format 'tag))
 
 (custom-set-variables '(loophole-kmacro-completing-key (kbd "C-]"))
                       '(loophole-bind-command-order
