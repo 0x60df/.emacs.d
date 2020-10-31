ad02d8cf54d4b2fd4d11e544df08fa675baffbbc
0x60df <0x60df@gmail.com>
2020-10-31 17:13:17 +0900

    Modify unit init-smex
    Remove unnecessary codes to eliminate load on init.
    Bind keys by overriding-set-key.

--- init.d/lisp/packages/init-smex.el
+++ init.d/lisp/packages/init-smex.el
@@ -2,25 +2,13 @@
 ;;;; init-smex.el
 
 
-
-;;; base
-
 (premise init)
+(premise bindings)
 (premise inst-smex)
 
-(smex-initialize)
 
-
-;;; bindings
-
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'execute-extended-command)
-
-(defadvice smex-prepare-ido-bindings (after
-                                      smex-prepare-additional-ido-bindings)
-  (define-key ido-completion-map " " 'ido-next-match)
-  (define-key ido-completion-map (kbd "S-SPC") 'ido-prev-match))
-(ad-activate 'smex-prepare-ido-bindings)
+(overriding-set-key (kbd "M-x") #'smex)
+(overriding-set-key (kbd "M-X") #'execute-extended-command)
 
 
 (resolve init-smex)
