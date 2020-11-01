c9da3b109d0e1ac424fcd4531f5016987e64e803
0x60df <0x60df@gmail.com>
2020-11-01 17:22:56 +0900

    Modify unit init-magit
    Bind key by overriding-set-key.
    Wrap codes by with-eval-after-load.

--- init.d/lisp/packages/init-magit.el
+++ init.d/lisp/packages/init-magit.el
@@ -2,20 +2,22 @@
 ;;;; init-magit.el
 
 
-
-;;; base
-
 (premise init)
+(premise bindings)
+(premise whitespace)
+(premise init-flyspell)
 (premise inst-magit)
 
-(global-set-key "\C-cvm" 'magit-status)
-(mapc (lambda (hook)
-        (add-hook hook (lambda ()
-                         (setq show-trailing-whitespace nil))))
-      '(magit-mode-hook
-        magit-popup-mode-hook))
+(declare-function git-commit-turn-on-flyspell "git-commit")
 
-(add-hook 'git-commit-setup-hook 'git-commit-turn-on-flyspell)
+(overriding-set-key "\C-cvm" #'magit-status)
+
+(with-eval-after-load 'magit-mode
+  (add-hook 'magit-mode-hook (lambda ()
+                               (setq show-trailing-whitespace nil))))
+
+(with-eval-after-load 'git-commit
+  (add-hook 'git-commit-setup-hook #'git-commit-turn-on-flyspell))
 
 
 (resolve init-magit)
