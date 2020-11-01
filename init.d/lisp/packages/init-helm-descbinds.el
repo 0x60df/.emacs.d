06894d8cbddf2cd06aa3e663bfca40c766ee8e1a
0x60df <0x60df@gmail.com>
2020-11-01 12:56:07 +0900

    Style unit init-helm-descbinds

--- init.d/lisp/packages/init-helm-descbinds.el
+++ init.d/lisp/packages/init-helm-descbinds.el
@@ -3,9 +3,10 @@
 
 
 (premise init)
+(premise init-helm)
 (premise inst-helm-descbinds)
 
-(define-key helm-command-map (kbd "h b") 'helm-descbinds)
+(define-key helm-command-map (kbd "h b") #'helm-descbinds)
 
 
 (resolve init-helm-descbinds)
