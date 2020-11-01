104fa6f54771777be81a311350d7066d49249621
0x60df <0x60df@gmail.com>
2020-11-01 12:51:22 +0900

    Style unit init-helm-ag

--- init.d/lisp/packages/init-helm-ag.el
+++ init.d/lisp/packages/init-helm-ag.el
@@ -3,9 +3,13 @@
 
 
 (premise init)
+(premise bindings)
+(premise init-helm)
+(premise init-ag)
 (premise inst-helm-ag)
 
-(define-key helm-command-map (kbd "C-a") 'helm-ag)
+
+(define-key helm-command-map (kbd "C-a") #'helm-ag)
 
 
 (resolve init-helm-ag)
