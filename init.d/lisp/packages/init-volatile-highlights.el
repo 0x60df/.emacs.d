0bbf0b5a1856560dea5208d0ef852101f17152a9
0x60df <0x60df@gmail.com>
2020-11-01 14:29:02 +0900

    Modify unit init-volatile-highlights
    Wrap volatile-highlights-mode by emacs-startup-hook.

--- init.d/lisp/packages/init-volatile-highlights.el
+++ init.d/lisp/packages/init-volatile-highlights.el
@@ -5,9 +5,10 @@
 (premise init)
 (premise inst-volatile-highlights)
 
-(volatile-highlights-mode 1)
-(eval-after-load 'volatile-highlights
-  '(setcdr (assq 'volatile-highlights-mode minor-mode-alist) '("")))
+(with-eval-after-load 'volatile-highlights
+  (setcdr (assq 'volatile-highlights-mode minor-mode-alist) '("")))
+
+(add-hook 'emacs-startup-hook #'volatile-highlights-mode)
 
 
 (resolve init-volatile-highlights)
