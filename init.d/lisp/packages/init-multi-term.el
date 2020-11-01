dc7bd84be5c66b563d57634760205b76ae58ffff
0x60df <0x60df@gmail.com>
2020-11-01 14:50:58 +0900

    Modify unit init-multi-term
    Bind keys by overriding-set-key.
    Add C-l to term-unbind-key-list.
    Wrap setting for term-unbind-key-list by if init-unit-p init-helm.
    Check duplication on setting term-bind-key-alist and
    term-unbind-key-list.
    Style codes.

--- init.d/lisp/packages/init-multi-term.el
+++ init.d/lisp/packages/init-multi-term.el
@@ -6,21 +6,40 @@
 (premise bindings)
 (premise inst-multi-term)
 
-(global-set-key (kbd "H-t") 'multi-term)
-(overriding-set-key (kbd "C-l t") 'multi-term)
-(eval-after-load 'multi-term
-  '(progn
-     (custom-set-variables
-      `(term-unbind-key-list
-        '("C-q" ,@term-unbind-key-list))) ;work with helm
-     (custom-set-variables
-      `(term-bind-key-alist
-        ',(append term-bind-key-alist
+(overriding-set-key (kbd "H-t") #'multi-term)
+(overriding-set-key (kbd "C-l t") #'multi-term)
+(with-eval-after-load 'multi-term
+  (custom-set-variables
+               '(term-unbind-key-list
+                 (append
+                  (seq-filter (lambda (unbind-key)
+                                (not (member unbind-key term-unbind-key-list)))
+                              '("C-l"))
+                  term-unbind-key-list)))
+  (custom-set-variables
+   '(term-bind-key-alist
+     (append
+      (seq-filter (lambda (bind-key)
+                    (not (member bind-key term-bind-key-alist)))
                   '(("C-c C-j" . term-line-mode)
                     ("C-p" . term-send-raw)
                     ("C-n" . term-send-raw)
                     ("C-r" . term-send-raw)
-                    ("C-s" . term-send-raw)))))))
+                    ("C-s" . term-send-raw)))
+      term-bind-key-alist))))
+
+(add-hook 'after-init-hook
+          (lambda ()
+            (with-eval-after-load 'multi-term
+              (if (init-unit-p init-helm)
+                  (custom-set-variables
+                   '(term-unbind-key-list
+                     (append
+                      (seq-filter (lambda (unbind-key)
+                                    (not (member unbind-key
+                                                 term-unbind-key-list)))
+                                  '("C-q"))
+                      term-unbind-key-list)))))))
 
 
 (resolve init-multi-term)
