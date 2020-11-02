3b40f8586dbd2ae401eb3514e45e86cbcd94854f
0x60df <0x60df@gmail.com>
2020-11-02 23:48:51 +0900

    Style unit init-rainbow-delimiters

--- init.d/lisp/packages/init-rainbow-delimiters.el
+++ init.d/lisp/packages/init-rainbow-delimiters.el
@@ -5,7 +5,7 @@
 (premise init)
 (premise inst-rainbow-delimiters)
 
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 
 
 (resolve init-rainbow-delimiters)
