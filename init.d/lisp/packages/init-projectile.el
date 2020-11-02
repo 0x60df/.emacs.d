19c3c955c588bbaf51e1900f179718f902160b96
0x60df <0x60df@gmail.com>
2020-11-02 21:29:01 +0900

    Modify unit init-projectile
    Update settings for mode-line.
    Bind keys by overriding-set-key.

--- init.d/lisp/packages/init-projectile.el
+++ init.d/lisp/packages/init-projectile.el
@@ -3,23 +3,22 @@
 
 
 (premise init)
+(premise custom)
+(premise bindings)
 (premise inst-projectile)
 
 (eval-when-compile (require 'projectile))
 
-(eval-after-load 'projectile
-  '(progn
-     (custom-set-variables
-      '(projectile-mode-line
-        '(:eval (if (file-remote-p default-directory)
-                    " P"
-                  (format " P[%s]" (projectile-project-name))))))
+(custom-set-variables
+ '(projectile-mode-line-prefix " P"))
 
-     (define-key projectile-command-map (kbd "p") 'projectile-mode)
-     (define-key projectile-command-map (kbd "SPC")
-       'projectile-switch-project)))
+(with-eval-after-load 'projectile
+  (setq-default projectile--mode-line projectile-mode-line-prefix)
+  (define-key projectile-command-map (kbd "p") #'projectile-mode)
+  (define-key projectile-command-map (kbd "SPC")
+    #'projectile-switch-project))
 
-(global-set-key (kbd "C-c p") 'projectile-mode)
+(overriding-set-key (kbd "C-c v P") #'projectile-mode)
 
 
 (resolve init-projectile)
