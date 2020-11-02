2a7fdab322edba31508b9e48189fefe99a4fc8d0
0x60df <0x60df@gmail.com>
2020-11-02 21:09:09 +0900

    Modify unit init-git-gutter-fringe
    Wrap loading statement by emacs-startup-hook.
    Bind keys by overriding-set-key.

--- init.d/lisp/packages/init-git-gutter-fringe.el
+++ init.d/lisp/packages/init-git-gutter-fringe.el
@@ -2,35 +2,41 @@
 ;;;; init-git-gutter-fringe.el
 
 
-
-;;; base
-
 (premise init)
+(premise custom)
+(premise bindings)
 (premise inst-git-gutter-fringe)
 
-(require 'git-gutter-fringe)
-(global-git-gutter-mode t)
-(eval-after-load 'git-gutter
-  '(custom-set-variables '(git-gutter:lighter " GG")))
+(declare-function git-gutter:popup-buffer-window "git-gutter")
+(declare-function git-gutter:previous-hunk "git-gutter")
+(declare-function git-gutter:next-hunk "git-gutter")
+(declare-function git-gutter:popup-hunk "git-gutter")
+(declare-function git-gutter:revert-hunk "git-gutter")
+(declare-function git-gutter:stage-hunk "git-gutter")
+(declare-function git-gutter:mark-hunk "git-gutter")
 
-
-;;; bindings
-
-(global-set-key "\C-cv<" 'git-gutter:previous-hunk)
-(global-set-key "\C-cv>" 'git-gutter:next-hunk)
-(global-set-key "\C-cvp" 'git-gutter:previous-hunk)
-(global-set-key "\C-cvn" 'git-gutter:next-hunk)
-(global-set-key "\C-cvd" 'git-gutter:popup-hunk)
-(global-set-key "\C-cvr" 'git-gutter:revert-hunk)
-(global-set-key "\C-cvs" 'git-gutter:stage-hunk)
-(global-set-key "\C-cv " 'git-gutter:mark-hunk)
-
-
-;;; functions
+(custom-set-variables
+ '(git-gutter:lighter " GG"))
 
 (defun git-gutter:close-popup ()
+  "Close git gutter popup window."
   (interactive)
   (delete-window (git-gutter:popup-buffer-window)))
+
+(overriding-set-key (kbd "C-c v q") #'git-gutter:close-popup)
+(overriding-set-key (kbd "C-c v <") #'git-gutter:previous-hunk)
+(overriding-set-key (kbd "C-c v >") #'git-gutter:next-hunk)
+(overriding-set-key (kbd "C-c v p") #'git-gutter:previous-hunk)
+(overriding-set-key (kbd "C-c v n") #'git-gutter:next-hunk)
+(overriding-set-key (kbd "C-c v d") #'git-gutter:popup-hunk)
+(overriding-set-key (kbd "C-c v r") #'git-gutter:revert-hunk)
+(overriding-set-key (kbd "C-c v s") #'git-gutter:stage-hunk)
+(overriding-set-key (kbd "C-c v SPC") #'git-gutter:mark-hunk)
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (require 'git-gutter-fringe)
+            (global-git-gutter-mode)))
 
 
 (resolve init-git-gutter-fringe)
