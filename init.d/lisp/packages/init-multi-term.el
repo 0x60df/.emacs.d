8774f8593c19a1c04fa1e4648ca37bd99753021d
0x60df <0x60df@gmail.com>
2020-11-14 21:09:14 +0900

    Modify unit init-multi-term
    Change setting procedure from custom-set-variables to add-to-list.

--- init.d/lisp/packages/init-multi-term.el
+++ init.d/lisp/packages/init-multi-term.el
@@ -9,37 +9,24 @@
 (overriding-set-key (kbd "H-t") #'multi-term)
 (overriding-set-key (kbd "C-l t") #'multi-term)
 (with-eval-after-load 'multi-term
-  (custom-set-variables
-               '(term-unbind-key-list
-                 (append
-                  (seq-filter (lambda (unbind-key)
-                                (not (member unbind-key term-unbind-key-list)))
-                              '("C-l"))
-                  term-unbind-key-list)))
-  (custom-set-variables
-   '(term-bind-key-alist
-     (append
-      (seq-filter (lambda (bind-key)
-                    (not (member bind-key term-bind-key-alist)))
-                  '(("C-c C-j" . term-line-mode)
-                    ("C-p" . term-send-raw)
-                    ("C-n" . term-send-raw)
-                    ("C-r" . term-send-raw)
-                    ("C-s" . term-send-raw)))
-      term-bind-key-alist))))
+  (add-to-list 'term-unbind-key-list "C-l")
+
+  (mapc (lambda (cell)
+          (let ((assoc (assoc (car cell) term-bind-key-alist)))
+            (if assoc
+                (setcdr assoc (cdr cell))
+              (setq term-bind-key-alist (cons cell term-bind-key-alist)))))
+        '(("C-c C-j" . term-line-mode)
+          ("C-p" . term-send-raw)
+          ("C-n" . term-send-raw)
+          ("C-r" . term-send-raw)
+          ("C-s" . term-send-raw))))
 
 (add-hook 'after-init-hook
           (lambda ()
             (with-eval-after-load 'multi-term
               (if (init-unit-p init-helm)
-                  (custom-set-variables
-                   '(term-unbind-key-list
-                     (append
-                      (seq-filter (lambda (unbind-key)
-                                    (not (member unbind-key
-                                                 term-unbind-key-list)))
-                                  '("C-q"))
-                      term-unbind-key-list)))))))
+                  (add-to-list 'term-unbind-key-list "C-q")))))
 
 
 (resolve init-multi-term)
