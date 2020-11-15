d7f9acd20564e8ac3969604e0c17f0636c96df9b
0x60df <0x60df@gmail.com>
2020-11-16 00:26:01 +0900

    Style unit init-cmuscheme

--- init.d/lisp/features/init-cmuscheme.el
+++ init.d/lisp/features/init-cmuscheme.el
@@ -4,8 +4,8 @@
 
 (premise init)
 
-(eval-after-load 'cmuscheme
-  '(add-hook 'inferior-scheme-mode-hook (lambda () (setq mode-name "InfScm"))))
+(with-eval-after-load 'cmuscheme
+  (add-hook 'inferior-scheme-mode-hook (lambda () (setq mode-name "InfScm"))))
 
 
 (resolve init-cmuscheme)
