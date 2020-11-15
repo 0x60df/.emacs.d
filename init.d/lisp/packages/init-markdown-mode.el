a7ae5ffd35cc8fa33767a874f2d48bcd945b6c05
0x60df <0x60df@gmail.com>
2020-11-15 23:15:30 +0900

    Add unit init-markdown-mode

---
+++ init.d/lisp/packages/init-markdown-mode.el
@@ -0,0 +1,25 @@
+
+;;;; init-markdown-mode.el
+
+
+(premise init)
+(premise whitespace)
+(premise inst-markdown-mode)
+
+(defun markdown-show-trailing-whitespace ()
+  "Show trailing whitespace."
+  (interactive)
+  (font-lock-add-keywords nil '(("[[:blank:]]+$" . 'trailing-whitespace)) t))
+
+(defun markdown-hide-trailing-whitespace ()
+  "Hide trailing whitespace."
+  (interactive)
+  (font-lock-remove-keywords nil '(("[[:blank:]]+$" . 'trailing-whitespace))))
+
+(with-eval-after-load 'markdown-mode
+  (add-hook 'markdown-mode-hook
+            (lambda () (setq show-trailing-whitespace nil)))
+  (add-hook 'markdown-mode-hook #'markdown-show-trailing-whitespace))
+
+
+(resolve init-markdown-mode)
