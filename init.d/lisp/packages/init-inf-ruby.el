ee8ba9eb71cf6189c82dba08cd801e03046a2f03
0x60df <0x60df@gmail.com>
2020-11-17 01:35:29 +0900

    Modify unit init-inf-ruby
    Remove setting for helm which is no longer used.

--- init.d/lisp/packages/init-inf-ruby.el
+++ init.d/lisp/packages/init-inf-ruby.el
@@ -19,12 +19,5 @@
 (with-eval-after-load 'ruby-mode
   (define-key ruby-mode-map (kbd "C-c c r") #'inf-ruby-other-window))
 
-;;; helm support
-
-(with-eval-after-load 'inf-ruby
-  (with-eval-after-load 'helm-mode
-    (add-to-list 'helm-completing-read-handlers-alist
-                 '(ruby-load-file . ido-read-file-name))))
-
 
 (resolve init-inf-ruby)
