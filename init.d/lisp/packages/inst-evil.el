3f62766fa43180dd81f3d424cf77b39d19858de6
0x60df <0x60df@gmail.com>
2020-11-17 00:58:51 +0900

    Modify unit inst-evil
    In order to suppress info, use arguments for el-get-bundle instead of
    user recipe.

--- init.d/lisp/packages/inst-evil.el
+++ init.d/lisp/packages/inst-evil.el
@@ -5,7 +5,17 @@
 (premise init)
 (premise init-el-get)
 
-(el-get-bundle evil-)
+(el-get-bundle evil
+  :build (list (delete "info" (car (plist-get
+                                    (el-get-read-recipe 'evil)
+                                    :build))))
+  :build/berkeley-unix (list (delete "info" (car (plist-get
+                                                  (el-get-read-recipe 'evil)
+                                                  :build/berkeley-unix))))
+  :build/darwin (list (delete "info" (car (plist-get
+                                           (el-get-read-recipe 'evil)
+                                           :build/darwin))))
+  :info nil)
 
 
 (resolve inst-evil)
