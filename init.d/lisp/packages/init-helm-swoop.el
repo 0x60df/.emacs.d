35c85470bc1c4e67d01b511c9edb91ce15761d42
0x60df <0x60df@gmail.com>
2020-12-05 16:30:46 +0900

    Modify unit init-helm-swoop
    Modify key bindings.

--- init.d/lisp/packages/init-helm-swoop.el
+++ init.d/lisp/packages/init-helm-swoop.el
@@ -8,7 +8,10 @@
 
 (define-key helm-command-map (kbd "C-s") #'helm-swoop)
 (with-eval-after-load 'isearch
-  (define-key isearch-mode-map (kbd "C-i") #'helm-swoop-from-isearch))
+  (define-key isearch-mode-map (kbd "C-q") (make-sparse-keymap))
+  (define-key isearch-mode-map (kbd "C-q C-q") #'isearch-quote-char)
+  (define-key isearch-mode-map (kbd "C-q C-s") #'helm-swoop-from-isearch)
+  (define-key isearch-mode-map (kbd "H-q") #'helm-swoop-from-isearch))
 
 
 (resolve init-helm-swoop)
