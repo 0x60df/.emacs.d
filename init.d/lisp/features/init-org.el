f3afafdafe7417340ab6ae498bbd7467605f3129
0x60df <0x60df@gmail.com>
2020-12-05 16:35:53 +0900

    Modify unit init-org
    Add setting for mode-line priority of org-indent.

--- init.d/lisp/features/init-org.el
+++ init.d/lisp/features/init-org.el
@@ -4,10 +4,16 @@
 
 (premise init)
 (premise custom)
+(premise mode-line)
 (premise bindings)
 
 (custom-set-variables
  '(org-use-speed-commands t))
+
+(push '(org-indent-mode . 1) mode-line-minor-mode-priority-alist)
+
+(with-eval-after-load 'org-indent
+  (setcar (cdr (assq 'org-indent-mode minor-mode-alist)) " in"))
 
 (overriding-set-key (kbd "C-c o a") #'org-agenda)
 (overriding-set-key (kbd "C-c o c") #'org-capture)
