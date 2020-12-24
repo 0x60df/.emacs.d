d2c88743ba56053f574afc06c1c986a185d8d901
0x60df <0x60df@gmail.com>
2020-12-24 18:47:56 +0900

    Modify unit inst-yester-theme
    Remove unnecessary codes, yester feature does not contain autoload.

--- init.d/lisp/packages/inst-yester-theme.el
+++ init.d/lisp/packages/inst-yester-theme.el
@@ -8,25 +8,6 @@
        (directory (concat user-emacs-directory "package/" name "-theme")))
   (add-to-list 'load-path directory)
 
-  (let ((el (locate-file name load-path '(".el")))
-        (elc (locate-file name load-path '(".elc"))))
-    (if (null el)
-        (error "Cannot find pacakge `%s'" name)
-      (unless (and elc (file-newer-than-file-p elc el))
-        (if elc (and (delete-file elc) (setq elc nil)))
-        (and (byte-compile-file el) (setq elc (byte-compile-dest-file el))))
-      (if (null elc)
-          (error "Connot byte compile pacakge `%s'" name))
-      (let ((loaddefs (replace-regexp-in-string "\\.el$" "-loaddefs.el" el)))
-        (unless (and (file-exists-p loaddefs)
-                     (file-newer-than-file-p loaddefs el))
-          (if (file-exists-p loaddefs) (delete-file loaddefs))
-          (update-file-autoloads el t loaddefs))
-        (unless (file-exists-p loaddefs)
-          (error "Connot update loaddefs for pacakge `%s'" name)))))
-
-  (require 'yester-loaddefs)
-
   (add-to-list 'custom-theme-load-path directory))
 
 
