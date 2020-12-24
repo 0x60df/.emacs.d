9785e978d22122b12d1d356a9c5bc8f61f12b817
0x60df <0x60df@gmail.com>
2020-12-24 20:41:37 +0900

    Fix unit inst-yester-theme
    Restore auto compiling codes.

--- init.d/lisp/packages/inst-yester-theme.el
+++ init.d/lisp/packages/inst-yester-theme.el
@@ -8,6 +8,16 @@
        (directory (concat user-emacs-directory "package/" name "-theme")))
   (add-to-list 'load-path directory)
 
+  (let ((el (locate-file name load-path '(".el")))
+        (elc (locate-file name load-path '(".elc"))))
+    (if (null el)
+        (error "Cannot find pacakge `%s'" name)
+      (unless (and elc (file-newer-than-file-p elc el))
+        (if elc (and (delete-file elc) (setq elc nil)))
+        (and (byte-compile-file el) (setq elc (byte-compile-dest-file el))))
+      (if (null elc)
+          (error "Connot byte compile pacakge `%s'" name))))
+
   (add-to-list 'custom-theme-load-path directory))
 
 
