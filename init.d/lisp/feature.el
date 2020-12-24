c98c52a9cf7d1bd46876c6c0997484aded33685e
0x60df <0x60df@gmail.com>
2020-12-24 18:49:54 +0900

    Modify unit feature
    Modify documentation string.

--- init.d/lisp/feature.el
+++ init.d/lisp/feature.el
@@ -7,10 +7,10 @@
 (defmacro idleload (function secs)
   "`autoload-do-load' FUNCTION by idle timer which is set by SECS.
 
-FUNCTION must be a symbol, which is embedded to lambda form
-for `run-with-idle-timer'. Hence, if any other form is used,
-it will be evaluated with global context when the timer is
-performed. It may cause unstable behavior.
+FUNCTION must be a quoted symbol, which is embedded into
+lambda form for `run-with-idle-timer'. Hence, if any other
+form is used, it will be evaluated with global context when
+the timer is performed. It may cause unstable behavior.
 SECS must be something number, which is valid for
 `run-with-idle-timer'."
   `(put ,function 'idleload
@@ -21,7 +21,7 @@ SECS must be something number, which is valid for
 (defmacro lazy-autoload (function file)
   "Define `autoload' for FUNCTION and FILE automatically.
 
-FUNCTION is a symbol to define autoload.
+FUNCTION is a quoted symbol to define autoload.
 FILE is a file name string for autoload.
 Rest optional arguments for autoload will be generated."
   (load file nil t)
