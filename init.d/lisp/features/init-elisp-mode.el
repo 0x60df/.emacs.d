b4200fcfe1b1d742d57cb27cab58539bd3518a8b
0x60df <0x60df@gmail.com>
2020-12-28 15:15:21 +0900

    Modify unit init-elisp-mode
    Add xref definitions for init unit.

--- init.d/lisp/features/init-elisp-mode.el
+++ init.d/lisp/features/init-elisp-mode.el
@@ -4,9 +4,34 @@
 
 (premise init)
 
+(eval-when-compile (require 'find-func))
+
+(declare-function elisp--xref-find-definitions-with-unit t t)
+
 (with-eval-after-load 'elisp-mode
   (add-hook 'lisp-interaction-mode-hook (lambda () (setq mode-name "Lisp I")))
-  (add-hook 'emacs-lisp-mode-hook (lambda () (setq mode-name "E-Lisp"))))
+  (add-hook 'emacs-lisp-mode-hook (lambda () (setq mode-name "E-Lisp")))
+
+  (with-eval-after-load 'find-func
+    (defconst find-unit-regexp ";;;; "
+      "The regexp used by `xref-find-definitions' for searching unit.")
+    (push '(unit . find-unit-regexp) find-function-regexp-alist))
+
+  (defun elisp--xref-find-definitions-with-unit (function symbol)
+    "Advising `elisp--xref-find-definitions' to add unit definition."
+    (let ((definitions (funcall function symbol))
+          (definitions-for-unit nil))
+      (mapc (lambda (prefix)
+              (let* ((s (intern (concat prefix (symbol-name symbol))))
+                     (unit (assq s init-units)))
+                (if unit
+                    (push (elisp--xref-make-xref 'unit (car unit) (cdr unit))
+                          definitions-for-unit))))
+            '("" "init-" "inst-"))
+      (append definitions definitions-for-unit)))
+  (advice-add 'elisp--xref-find-definitions
+              :around #'elisp--xref-find-definitions-with-unit))
+
 
 
 (resolve init-elisp-mode)
