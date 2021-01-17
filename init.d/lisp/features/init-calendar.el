fc3bb78d0e653c7f856b53feeed87ac89b3fe4d8
0x60df <0x60df@gmail.com>
2021-01-17 21:10:24 +0900

    Modify unit init-calendar
    Add settings to mark today and holidays.

--- init.d/lisp/features/init-calendar.el
+++ init.d/lisp/features/init-calendar.el
@@ -4,9 +4,17 @@
 
 (premise init)
 
+(declare-function calendar-mark-today "calendar")
+
+(custom-set-variables
+ '(calendar-mark-holidays-flag t))
+
 (with-eval-after-load 'calendar
   (add-hook 'calendar-mode-hook
             (lambda () (setq show-trailing-whitespace nil))))
+
+(with-eval-after-load 'calendar
+  (add-hook 'calendar-today-visible-hook #'calendar-mark-today))
 
 
 (resolve init-calendar)
