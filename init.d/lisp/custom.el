ab7eecc0bd3a4079abe2f1c3f9204cb9b2db9839
0x60df <0x60df@gmail.com>
2021-01-17 17:58:24 +0900

    Modify unit custom
    Add settings for appearance.

--- init.d/lisp/custom.el
+++ init.d/lisp/custom.el
@@ -7,7 +7,8 @@
 (defgroup user nil "Group for user customization" :group 'emacs)
 
 (custom-set-variables
- '(custom-file (concat user-emacs-directory "custom.el")))
+ '(custom-file (concat user-emacs-directory "custom.el"))
+ '(custom-magic-show-button t))
 (add-hook 'after-init-hook (lambda () (load custom-file 'noerror)))
 
 
