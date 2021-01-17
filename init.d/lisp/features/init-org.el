24608a932fe20c333d3ff0a3fed80d123619e0d7
0x60df <0x60df@gmail.com>
2021-01-17 17:04:48 +0900

    Modify unit init-org
    Add Advice for hiding trailing whitespace on the org export
    dispatcher buffer.

--- init.d/lisp/features/init-org.el
+++ init.d/lisp/features/init-org.el
@@ -16,6 +16,15 @@
 
 (push '(org-indent-mode . 1) mode-line-minor-mode-priority-alist)
 
+(defun org-hide-trailing-whitespace-for-export-dispatcher (buffer &rest args)
+  "Advising `org-switch-to-buffer-other-window' to hide whitespace."
+  (if (eq buffer (get-buffer "*Org Export Dispatcher*"))
+      (with-current-buffer "*Org Export Dispatcher*"
+        (setq show-trailing-whitespace nil))))
+
+(advice-add 'org-switch-to-buffer-other-window
+            :before #'org-hide-trailing-whitespace-for-export-dispatcher)
+
 (with-eval-after-load 'org-indent
   (setcar (cdr (assq 'org-indent-mode minor-mode-alist)) " in"))
 
