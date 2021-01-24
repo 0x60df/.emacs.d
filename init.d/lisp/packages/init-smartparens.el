2e7ac6433f52df79167dc5d0ff0fcf32007d634c
0x60df <0x60df@gmail.com>
2021-01-24 17:02:53 +0900

    Modify unit init-smartparens
    Add function sp-show-set-region.
    
    Change variable name from sp-show--enclosing-pair-level to
    sp-show-enclosing-pair-level.

--- init.d/lisp/packages/init-smartparens.el
+++ init.d/lisp/packages/init-smartparens.el
@@ -23,29 +23,28 @@
 
 (push '(smartparens-mode . 13) mode-line-minor-mode-priority-alist)
 
-(defvar sp-show--enclosing-pair-level 0
-  "Level for highlighting enclosing pari.")
+(defvar sp-show-enclosing-pair-level 0 "Level for highlighting enclosing pair.")
 
 (define-minor-mode sp-show-enclosing-pair-mode
-    "Minor mode for enhancing `sp-show-enclosing-pair'."
-    nil
-    nil
-    '(("p" . sp-show-enclosing-pair-dwim)
-      ("P" . sp-show-enclosing-pair-dwim-reverse)
-      ("(" . sp-show-enclosing-pair-jump-backward)
-      (")" . sp-show-enclosing-pair-jump-forward)
-      ([?\C-b] . sp-show-enclosing-pair-jump-backward)
-      ([?\C-f] . sp-show-enclosing-pair-jump-forward)))
+  "Minor mode for enhancing `sp-show-enclosing-pair'."
+  nil
+  nil
+  '(("p" . sp-show-enclosing-pair-dwim)
+    ("P" . sp-show-enclosing-pair-dwim-reverse)
+    ("(" . sp-show-enclosing-pair-jump-backward)
+    (")" . sp-show-enclosing-pair-jump-forward)
+    ([?\C-b] . sp-show-enclosing-pair-jump-backward)
+    ([?\C-f] . sp-show-enclosing-pair-jump-forward)
+    ("m" . sp-show-set-region)))
 
 (defun sp-show-enclosing-pair-dwim (&optional arg)
   "`sp-show-enclosing-pair' with auto upgrading the level."
   (interactive "p")
 
   (sp-show-enclosing-pair-mode)
-  (setq sp-show--enclosing-pair-level (+ arg sp-show--enclosing-pair-level))
+  (setq sp-show-enclosing-pair-level (+ arg sp-show-enclosing-pair-level))
   (advice-add-for-once
    'sp-show--pair-enc-function
-   ;; Copied from `smartparens'
    :before (lambda (&rest args)
              (advice-add-for-once
               'sp-get-enclosing-sexp
@@ -53,47 +52,55 @@
                         (or (funcall
                              function
                              (or args
-                                 (if (< 0 sp-show--enclosing-pair-level)
-                                     sp-show--enclosing-pair-level
-                                   (setq sp-show--enclosing-pair-level 1))))
+                                 (if (< 0 sp-show-enclosing-pair-level)
+                                     sp-show-enclosing-pair-level
+                                   (setq sp-show-enclosing-pair-level 1))))
                             (progn
-                              (setq sp-show--enclosing-pair-level 1)
+                              (setq sp-show-enclosing-pair-level 1)
                               (funcall function))))))))
-
-(defun sp-show-enclosing-pair-dwim-reverse (&optional arg)
-  "`sp-show-enclosing-pair-dwim' by reverse direction."
-  (interactive "p")
-  (sp-show-enclosing-pair-dwim (- arg)))
-
-(defun sp-show-enclosing-pair-jump-forward ()
-  "Jump to the highlighted closing pair."
-  (interactive)
-  (when sp-show-pair-enc-overlays
-    (goto-char (overlay-end (cdr sp-show-pair-enc-overlays)))))
-
-(defun sp-show-enclosing-pair-jump-backward ()
-  "Jump to the lighlighted opening pair."
-  (interactive)
-  (when sp-show-pair-enc-overlays
-    (goto-char (overlay-start (car sp-show-pair-enc-overlays)))))
 
 (advice-add-for-once 'sp-show-enclosing-pair-dwim
                      :before (lambda (&rest args) (require 'smartparens)))
 
 (with-eval-after-load 'smartparens
-  (mapc (lambda (function)
-          (add-to-list 'sp-show-enclosing-pair-commands function))
-        '(sp-show-enclosing-pair-dwim sp-show-enclosing-pair-dwim-reverse))
+  (defun sp-show-enclosing-pair-dwim-reverse (&optional arg)
+  "`sp-show-enclosing-pair-dwim' by reverse direction."
+  (interactive "p")
+  (sp-show-enclosing-pair-dwim (- arg)))
+
+  (defun sp-show-enclosing-pair-jump-forward ()
+    "Jump to the highlighted closing pair."
+    (interactive)
+    (when sp-show-pair-enc-overlays
+      (goto-char (overlay-end (cdr sp-show-pair-enc-overlays)))))
+
+  (defun sp-show-enclosing-pair-jump-backward ()
+    "Jump to the lighlighted opening pair."
+    (interactive)
+    (when sp-show-pair-enc-overlays
+      (goto-char (overlay-start (car sp-show-pair-enc-overlays)))))
+
+  (defun sp-show-set-region ()
+    "Set region by enclosing."
+    (interactive)
+    (let ((point (overlay-start (car sp-show-pair-enc-overlays)))
+          (mark (overlay-end (cdr sp-show-pair-enc-overlays))))
+      (goto-char point)
+      (push-mark mark t t)))
 
   (defun sp-show--enclosing-pair-cleanup (&rest args)
-    "Advising function for resetting `sp-show--enclosing-pair-level'"
+    "Advising function for resetting `sp-show-enclosing-pair-level'"
     (unless (and (memq last-command '(sp-show-enclosing-pair-dwim
                                       sp-show-enclosing-pair-dwim-reverse))
                  (memq this-command '(sp-show-enclosing-pair-dwim
                                       sp-show-enclosing-pair-dwim-reverse
                                       nil)))
       (sp-show-enclosing-pair-mode 0)
-      (setq sp-show--enclosing-pair-level 0)))
+      (setq sp-show-enclosing-pair-level 0)))
+
+  (mapc (lambda (function)
+          (add-to-list 'sp-show-enclosing-pair-commands function))
+        '(sp-show-enclosing-pair-dwim sp-show-enclosing-pair-dwim-reverse))
 
   (advice-add 'sp-show--pair-delete-enc-overlays
               :after #'sp-show--enclosing-pair-cleanup))
