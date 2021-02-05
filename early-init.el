0fdae9699d2db7380e7826a5d3d0c3dbc2d5e9a5
0x60df <0x60df@gmail.com>
2021-02-05 20:23:49 +0900

    Add early-init.el and modify .gitignore

---
+++ early-init.el
@@ -0,0 +1,8 @@
+
+;;;; early-init.el
+
+
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars . nil) default-frame-alist)
+(push '(horizontal-scroll-bars . nil) default-frame-alist)
