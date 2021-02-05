feabb1048bff527e80da3f1cbf53a1a165ff3612
0x60df <0x60df@gmail.com>
2021-02-05 20:19:17 +0900

    Modify unit advice and subr
    Modify functions to get an optional argument trigger, which delays
    running function.

--- init.d/lisp/advice.el
+++ init.d/lisp/advice.el
@@ -5,7 +5,7 @@
 (premise init)
 (premise print)
 
-(defun advice-add-for-once (symbol where function &optional props)
+(defun advice-add-for-once (symbol where function &optional props trigger)
   "`advice-add' for one time use.
 
 Advising function is not a FUNCTION but uninterned symbol,
@@ -13,27 +13,45 @@ Advising function is not a FUNCTION but uninterned symbol,
 containing FUNCTION and `advice-remove' for
 #:transient-advice-function.
 SYMBOL, WHERE, and PROPS are passed to `advice-add' and
-`advice-remove'."
+`advice-remove'.
+When TRIGGER is non-nil, running FUNCTION and
+`advice-remove' is delayed until funcall TRIGGER returns
+non-nil."
   (let ((advice (make-symbol "transient-advice-function")))
     (fset advice `(lambda (&rest args)
-                    (unwind-protect
-                        (apply ',function args)
-                      (advice-remove ',symbol ',advice))))
+                    (if (or ,(null trigger) (funcall ',trigger))
+                        (unwind-protect
+                            (apply ',function args)
+                          (advice-remove ',symbol ',advice)))))
     (advice-add symbol where advice props)))
 
-(defun advice-remove-for-once (symbol function)
+(defun advice-remove-for-once (symbol function &optional trigger)
   "`advice-remove' for the function whose body is FUNCTION.
-Find the function for the SYMBOL, and `advice-remove' it."
+Find the function for the SYMBOL, and `advice-remove' it.
+
+When optional argument TRIGGER is non-nil, `advice-remove'
+is performed only if `transient-advice-function' is added
+with trigger function which equals to TRIGGER.
+On the other hand, `transient-advice-function' with trigger
+can be removed only if TRIGGER equals to the trigger."
   (let (advice)
     (advice-mapc (lambda (f p)
                    (if (and (symbolp f)
                             (equal (symbol-name f) "transient-advice-function")
                             (not (eq f (intern "transient-advice-function")))
-                            (equal (cadr (cadr (caddr (symbol-function f))))
+                            (if trigger
+                                (equal (cadr (caddr (cadr
+                                                     (caddr
+                                                      (symbol-function f)))))
+                                       `',trigger)
+                              (cadr (cadr (caddr (symbol-function f)))))
+                            (equal (cadr (cadr (caddr (caddr
+                                                       (symbol-function f)))))
                                    `',function))
                        (setq advice f)))
                  symbol)
-    (advice-remove symbol advice)))
+    (if advice                          ; test advice to avoid remove all, BUG?
+        (advice-remove symbol advice))))
 
 
 (resolve advice)
--- init.d/lisp/subr.el
+++ init.d/lisp/subr.el
@@ -9,31 +9,47 @@
   "Message ARGS delimited by space."
   (apply #'message (mapconcat (lambda (a) [?% ?s]) args " ") args))
 
-(defun add-hook-for-once (hook function &optional depth local)
+(defun add-hook-for-once (hook function &optional depth local trigger)
   "Add to the value of HOOK the function FUNCTION for once use.
 
 Hooked function is not a FUNCTION but uninterned symbol,
 `transient-hook-function', which stores lambda form
 containing FUNCTION and `remove-hook' for
 #:transient-hook-function.
-DEPTH and LOCAL are passed to `add-hook' and `remove-hook'."
+DEPTH and LOCAL are passed to `add-hook' and `remove-hook'.
+When TRIGGER is non-nil, running FUNCTION and `remove-hook'
+is delayed until funcall TRIGGER returns non-nil."
   (let ((symbol (make-symbol "transient-hook-function")))
     (fset symbol `(lambda ()
-                    (unwind-protect
-                        (funcall ',function)
-                      (remove-hook ',hook ',symbol ',local))))
+                    (if (or ,(null trigger) (funcall ',trigger))
+                        (unwind-protect
+                            (funcall ',function)
+                          (remove-hook ',hook ',symbol ',local)))))
     (add-hook hook symbol depth local)))
 
-(defun remove-hook-for-once (hook function &optional local)
-  "Remove from the value of HOOK the function whose body is FUNCTION."
+(defun remove-hook-for-once (hook function &optional local trigger)
+  "Remove from the value of HOOK the function whose body is FUNCTION.
+
+Optional argument LOCAL is passed to `remove-hook'.
+
+When optional argument TRIGGER is non-nil, `remove-hook' is
+performed only if `transient-hook-function' is added with
+trigger function which equals to TRIGGER.
+On the other hand, `transient-hook-function' with trigger
+can be removed only if TRIGGER equals to the trigger."
   (let ((symbol (seq-find
                  (lambda (f)
                    (and (symbolp f)
                         (equal (symbol-name f) "transient-hook-function")
                         (not (eq f (intern "transient-hook-function")))
-                        (equal (cadr (cadr (caddr (symbol-function f))))
+                        (if trigger
+                            (equal (cadr (caddr (cadr (caddr
+                                                       (symbol-function f)))))
+                                   `',trigger)
+                          (cadr (cadr (caddr (symbol-function f)))))
+                        (equal (cadr (cadr (caddr (caddr (symbol-function f)))))
                                `',function)))
-                          (symbol-value hook))))
+                 (symbol-value hook))))
     (remove-hook hook symbol local)))
 
 
