15186b4fca4332f7817df5d9ed948276fa060015
0x60df <0x60df@gmail.com>
2021-02-06 14:09:46 +0900

    Modify unit init-ace-jump-mode
    Modify move keys and bindings.

--- init.d/lisp/packages/init-ace-jump-mode.el
+++ init.d/lisp/packages/init-ace-jump-mode.el
@@ -7,7 +7,12 @@
 (premise bindings)
 (premise inst-ace-jump-mode)
 
+(eval-when-compile (require 'ace-jump-mode))
+
 (push '(ace-jump-mode . 22) mode-line-minor-mode-priority-alist)
+
+(with-eval-after-load 'ace-jump-mode
+  (setq ace-jump-mode-move-keys (loop for i from ?a to ?z collect i)))
 
 (overriding-set-key (kbd "M-g M-j") #'ace-jump-mode)
 (overriding-set-key (kbd "H-g") #'ace-jump-mode)
@@ -30,10 +35,13 @@
                      ,(format "Ace jump word to `%s'." s)
                      (interactive)
                      (funcall-interactively 'ace-jump-word-mode ,c)))
-            (overriding-set-key (kbd (concat "A-C-" s)) jump-word-func))))
+            (overriding-set-key (kbd (concat "A-M-" s)) jump-word-func))))
       (vconcat "!\"#$%&'()*+,-./0123456789:;<=>?@"
                "[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"))
-(overriding-set-key (read-kbd-macro "A-<return>") #'ace-jump-line-mode)
+(overriding-set-key (kbd "A-<return>") #'ace-jump-line-mode)
+(with-eval-after-load 'iso-transl
+  (define-key key-translation-map (vector (logior ?\s ?\A-\^@)) nil))
+(overriding-set-key (kbd "A-SPC") #'ace-jump-line-mode)
 
 
 (resolve init-ace-jump-mode)
