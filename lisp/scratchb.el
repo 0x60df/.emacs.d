9f34eaec79c892bbb1bd8eafa0265d97d0e05731
0x60df <0x60df@gmail.com>
2021-02-06 23:27:56 +0900

    Modify user feature scratchb
    Add variable check for delete-file.

--- lisp/scratchb.el
+++ lisp/scratchb.el
@@ -65,10 +65,14 @@
                               "/"
                               (format-time-string "%Y%m%d%H%M%S.el"
                                                   (current-time))))))
-    (mapc #'delete-file (nthcdr scratchb-snapshot-limit
-                                (reverse (sort (file-expand-wildcards
-                                                "~/.emacs.d/scratchb/*.el")
-                                               #'string<))))))
+    (mapc (lambda (file)
+            (if (and (stringp file)
+                     (file-exists-p file))
+                (delete-file file)))
+          (nthcdr scratchb-snapshot-limit
+                  (reverse (sort (file-expand-wildcards
+                                  "~/.emacs.d/scratchb/*.el")
+                                 #'string<))))))
 
 (defun scratchb--snapshot-when-scratchb ()
   "Snapshot scratchb when current-frame is *scratch* buffer."
