7d152a818a94dca10bfeadad3c098b8a91335095
0x60df <0x60df@gmail.com>
2021-02-14 16:16:39 +0900

    Modify unit init-term
    Add settings for hl-line-mode.
    Turn it off locally during term-char-mode.

--- init.d/lisp/features/init-term.el
+++ init.d/lisp/features/init-term.el
@@ -5,7 +5,12 @@
 (premise init)
 
 (with-eval-after-load 'term
-  (add-hook 'term-mode-hook (lambda () (setq show-trailing-whitespace nil))))
+  (add-hook 'term-mode-hook (lambda () (setq show-trailing-whitespace nil)))
+  (advice-add 'term-char-mode :after (lambda (&rest args)
+                                       (setq-local global-hl-line-mode nil)
+                                       (global-hl-line-unhighlight)))
+  (advice-add 'term-line-mode :after (lambda (&rest args)
+                                       (setq-local global-hl-line-mode t))))
 
 
 (resolve init-term)
