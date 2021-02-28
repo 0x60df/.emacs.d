3c768843c2b0f4c6dde3dbcfd92a0c1fda3bd42d
0x60df <0x60df@gmail.com>
2021-02-28 21:56:58 +0900

    Modify unit init-eglot
    Add toggle entry point and bindings.

--- init.d/lisp/packages/init-eglot.el
+++ init.d/lisp/packages/init-eglot.el
@@ -3,12 +3,26 @@
 
 
 (premise init)
+(premise feature)
+(premise bindings)
 (premise inst-eglot)
 
 (eval-when-compile (require 'eglot))
 
+(lazy-autoload 'eglot-current-server "eglot")
+(lazy-autoload 'eglot-shutdown "eglot")
+
 (with-eval-after-load 'eglot
   (add-to-list 'eglot-stay-out-of 'company))
+
+(defun eglot-toggle ()
+    "Toggle eglot"
+    (interactive)
+    (if (eglot-current-server)
+        (call-interactively #'eglot-shutdown)
+      (call-interactively #'eglot)))
+
+(overriding-set-key (kbd "C-c c l") #'eglot-toggle)
 
 
 (resolve init-eglot)
