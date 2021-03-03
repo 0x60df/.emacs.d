bdc37cffb7779c1bb664f63d5ba5776bb6eee075
0x60df <0x60df@gmail.com>
2021-03-03 21:50:33 +0900

    Modify unit init-page-break-lines
    Shrink page-break-lines while company-pseudo-tooltip is visible.

--- init.d/lisp/packages/init-page-break-lines.el
+++ init.d/lisp/packages/init-page-break-lines.el
@@ -4,10 +4,35 @@
 
 (premise init)
 (premise custom)
+(premise init-company)
 (premise inst-page-break-lines)
+
+(eval-when-compile
+  (require 'page-break-lines)
+  (require 'company))
+
+(declare-function page-break-lines--update-display-tables "page-break-lines")
 
 (custom-set-variables
  '(page-break-lines-lighter ""))
+
+(with-eval-after-load 'company
+  (advice-add 'company-pseudo-tooltip-unless-initial-inline-frontend
+              :before
+              (lambda (command &rest args)
+                (when (and (arrayp buffer-display-table)
+                           (eq command 'post-command)
+                           (and company-status
+                                (not (eq company-status 'expanded))))
+                  (aset buffer-display-table ?\^L
+                        (vconcat (make-list company-minimum-prefix-length
+                                            (make-glyph-code
+                                             page-break-lines-char
+                                             'page-break-lines))))
+                  (add-hook-for-once
+                   'company-after-completion-hook
+                   (lambda (&rest args)
+                     (page-break-lines--update-display-tables)))))))
 
 (add-hook 'emacs-startup-hook #'global-page-break-lines-mode)
 
