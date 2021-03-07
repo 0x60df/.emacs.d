66c645dd67218342ca92dfd4daa3ccb674185129
0x60df <0x60df@gmail.com>
2021-03-07 21:56:17 +0900

    Modify unit init-scratchb
    Add timer for taking snapshot of scratch buffer automatically.

--- init.d/lisp/user-features/init-scratchb.el
+++ init.d/lisp/user-features/init-scratchb.el
@@ -12,6 +12,11 @@
 (add-hook 'emacs-startup-hook #'scratchb-mode-buffer-sticky)
 
 (with-eval-after-load 'scratchb
+  (defvar scratchb-auto-snapshot-timer nil
+    "Timer for taking snapshot of *scratchb* automatically.")
+  (setq scratchb-auto-snapshot-timer
+        (run-with-idle-timer (* 60 62) t #'scratchb-snapshot))
+
   (add-hook 'scratchb-after-revert-hook #'scratchb-mode-buffer-sticky)
 
   (define-key scratchb-mode-map (kbd "C-c k") #'scratchb-flush))
