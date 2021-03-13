2356009e49052358cae6f5399b96e275d41c22df
0x60df <0x60df@gmail.com>
2021-03-14 00:36:55 +0900

    Refactor user feature scratch
    Refine quitting auto snapshot mode.

--- lisp/scratch.el
+++ lisp/scratch.el
@@ -80,15 +80,12 @@ This function is intended to be used only in
       (progn
         (add-hook 'after-change-functions
                   #'scratch--setup-auto-snapshot-timer nil t)
-        (add-to-list 'write-contents-functions
-                     #'scratch--try-quit-auto-snapshot)
+        (add-hook 'before-save-hook #'scratch--try-quit-auto-snapshot nil t)
         (add-hook 'scratch-before-shred-hook #'scratch-snapshot nil t)
         (add-hook 'scratch-before-label-hook #'scratch-snapshot nil t))
     (remove-hook 'after-change-functions
                  #'scratch--setup-auto-snapshot-timer t)
-    (setq write-contents-functions
-          (delq #'scratch--try-quit-auto-snapshot
-                write-contents-functions))
+    (remove-hook 'before-save-hook #'scratch--try-quit-auto-snapshot t)
     (remove-hook 'scratch-before-shred-hook #'scratch-snapshot t)
     (remove-hook 'scratch-before-label-hook #'scratch-snapshot t)))
 
@@ -137,29 +134,22 @@ END and RANGE.  However, all of them are ignored."
                 (setq scratch-auto-snapshot-timer nil)))))))
 
 (defun scratch--try-quit-auto-snapshot ()
-  "Try quit auto snapshot.
-If scratch does not visit file until `pre-command-hook',
-revert auto snapshot.  Otherwise, do quit auto snapshot.
-This function is intended to be added to
-`write-contents-functions' as pre process.
-Therefore, return nil if successful."
-  (add-hook 'pre-command-hook #'scratch--revert-or-quit-auto-snapshot nil t)
-  (remove-hook 'after-change-functions #'scratch--setup-auto-snapshot-timer t)
-  nil)
+  "Try quit auto snapshot on `before-save-hook'.
+Disable `scratch-auto-snapshot-mode' and setup follow up
+function `scratch--revert-auto-snapshot' on
+`pre-command-hook' locally, which revert auto-snapshot.
+If file is saved successfully,
+`scratch--revert-auto-snapshot' will be discarded because
+local variables including `pre-command-hook' will be killed."
+  (add-hook 'pre-command-hook #'scratch--revert-auto-snapshot nil t)
+  (scratch-auto-snapshot-mode 0))
 
-(defun scratch--revert-or-quit-auto-snapshot ()
-  "Revert or quit auto snapshot according to the state of visit.
-If scratch does not visit file when this function is called,
-revert auto snapshot.  Otherwise, do quit auto snapshot.
+(defun scratch--revert-auto-snapshot ()
+  "Revert auto snapshot according to the state of visit.
 This function is intended to be added to `pre-command-hook'."
   (unwind-protect
-      (if buffer-file-name
-          (setq write-contents-functions
-                (delq #'scratch--try-quit-auto-snapshot
-                      write-contents-functions))
-        (add-hook 'after-change-functions
-                  #'scratch--setup-auto-snapshot-timer nil t))
-    (remove-hook 'pre-command-hook #'scratch--revert-or-quit-auto-snapshot t)))
+      (scratch-auto-snapshot-mode)
+    (remove-hook 'pre-command-hook #'scratch--revert-auto-snapshot t)))
 
 (defun scratch-shred (&optional scratch)
   "Kill SCRATCH buffer.
@@ -192,32 +182,29 @@ If SCRATCH is nil, lable current `scratch' buffer."
           (setq scratch-list (delq s scratch-list))
           (add-to-list 'scratch-labeled-list s)
 
-          (let ((auto-snapshot-flag scratch-auto-snapshot-mode))
-            (if auto-snapshot-flag (scratch-auto-snapshot-mode 0))
-            (if (or (null write-contents-functions)
-                    (yes-or-no-p "Override write-contents-functions?: "))
-                (setq write-contents-functions
-                      (cons (lambda ()
-                              (let ((directory (read-directory-name
-                                                "Directory to save in: ")))
-                                (if (not (file-writable-p directory))
-                                    (user-error
-                                     "Specified directory is not writable")
-                                  (let ((wcf write-contents-functions))
-                                    (unwind-protect
-                                        (progn
-                                          (setq write-contents-functions nil)
-                                          (write-file (concat directory
-                                                              (buffer-name)))
-                                          (setq scratch-labeled-list
-                                                (delq (current-buffer)
-                                                      scratch-labeled-list))
-                                          (setq wcf nil)
-                                          t)
-                                      (if wcf (setq write-contents-functions
-                                                    wcf)))))))
-                            write-contents-functions)))
-            (if auto-snapshot-flag (scratch-auto-snapshot-mode)))))))
+          (if (or (null write-contents-functions)
+                  (yes-or-no-p "Override write-contents-functions?: "))
+              (setq write-contents-functions
+                    (cons (lambda ()
+                            (let ((directory (read-directory-name
+                                              "Directory to save in: ")))
+                              (if (not (file-writable-p directory))
+                                  (user-error
+                                   "Specified directory is not writable")
+                                (let ((wcf write-contents-functions))
+                                  (unwind-protect
+                                      (progn
+                                        (setq write-contents-functions nil)
+                                        (write-file (concat directory
+                                                            (buffer-name)))
+                                        (setq scratch-labeled-list
+                                              (delq (current-buffer)
+                                                    scratch-labeled-list))
+                                        (setq wcf nil)
+                                        t)
+                                    (if wcf (setq write-contents-functions
+                                                  wcf)))))))
+                          write-contents-functions)))))))
 
 ;;;###autoload
 (defun scratch ()
