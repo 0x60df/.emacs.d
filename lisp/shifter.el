f473ba6dbd1ad164ee1c968af98202777324a2e6
0x60df <0x60df@gmail.com>
2021-03-22 01:28:03 +0900

    Modify user feature shifter
    Implement optional arguments for shift-minor-mode instead of isolated
    functions to turn-on and turn-off.

--- lisp/shifter.el
+++ lisp/shifter.el
@@ -4,8 +4,8 @@
 
 ;;; Code:
 
+(require 'seq)
 (require 'fmmm)
-(require 'cl-lib)
 
 (defgroup shifter nil
   "Shift major/minor-mode."
@@ -59,11 +59,50 @@
       (funcall s))))
 
 ;;;###autoload
-(defun shifter-shift-minor-mode ()
-  "Shift minor mode."
-  (interactive)
-  (let ((l (fmmm-minor-mode-list))
-        (h shifter-minor-mode-hist))
+(defun shifter-shift-minor-mode (&optional shift force)
+  "Shift minor mode.
+
+SHIFT has same role as an argument for general minor-mode.
+If SHIFT is quoted toggle, toggle minor-mode.
+If SHIFT is zero or negative integer, disable minor-mode.
+Otherwise, enable minor-mode.
+
+FORCE takes effect if SHIFT is other than quoted toggle.
+If FORCE is non-nil, this function lists up candidates and
+perform operation according to SHIFT unconditionally.
+Otherwise, counterparts will be listed up for candidates,
+that is to say, disabled modes for enabling, enabled modes
+for disabling."
+  (interactive `(,(cond ((null current-prefix-arg) 'toggle)
+                        ((< (prefix-numeric-value current-prefix-arg) 1) 0)
+                        (t nil))
+                 ,(if (or (and (consp current-prefix-arg)
+                               (let ((num (car current-prefix-arg)))
+                                 (or (< 4 num)
+                                     (< num -1))))
+                          (and (integerp current-prefix-arg)
+                               (or (< 9 current-prefix-arg)
+                                   (< current-prefix-arg 0))))
+                      t)))
+  (let* ((l (cond ((eq shift 'toggle) (fmmm-minor-mode-list))
+                  ((and (integerp shift) (< shift 1) (not force))
+                   (fmmm-enabled-minor-mode-list))
+                  ((not force) (fmmm-disabled-minor-mode-list))
+                  (t (fmmm-minor-mode-list))))
+         (h (funcall
+             (cond ((eq shift 'toggle) #'identity)
+                  ((and (integerp shift) (< shift 1) (not force))
+                   (lambda (hist)
+                     (let ((enabled-list (fmmm-enabled-minor-mode-list)))
+                       (seq-filter (lambda (e) (memq e enabled-list))
+                                   hist))))
+                  ((not force)
+                   (lambda (hist)
+                     (let ((enabled-list (fmmm-enabled-minor-mode-list)))
+                       (seq-filter (lambda (e) (not (memq e enabled-list)))
+                                   hist))))
+                  (t #'identity))
+             shifter-minor-mode-hist)))
     (mapc (lambda (s) (setq l (delq s l))) h)
     (let ((s (intern (completing-read "Minor mode: "
                                       (mapcar 'symbol-name (append h l))))))
@@ -80,63 +119,7 @@
       (when (fmmm-minor-mode-p s)
         (setq shifter-minor-mode-hist (delq s shifter-minor-mode-hist))
         (add-to-list 'shifter-minor-mode-hist s))
-      (funcall s 'toggle))))
-
-;;;###autoload
-(defun shifter-turn-on-minor-mode (force)
-  "Turn on minor mode.
-If FORCE is non-nil, read and enable minor mode regardless
-of its state."
-  (interactive "P")
-  (let ((l (if force
-               (fmmm-minor-mode-list)
-             (fmmm-disabled-minor-mode-list)))
-        (h (funcall (if force
-                        'identity
-                      (lambda (l)
-                        (cl-remove-if
-                         (lambda (s) (memq s (fmmm-enabled-minor-mode-list)))
-                         l)))
-                    shifter-minor-mode-hist)))
-    (mapc (lambda (s) (setq l (delq s l))) h)
-    (let ((s (intern (completing-read "Minor mode: "
-                                      (mapcar 'symbol-name (append h l))))))
-      (let ((f (symbol-function s)))
-        (if (autoloadp f) (autoload-do-load f)))
-      (if (not (fmmm-minor-mode-p s))
-          (setq s 'ignore))
-      (when (fmmm-minor-mode-p s)
-        (setq shifter-minor-mode-hist (delq s shifter-minor-mode-hist))
-        (add-to-list 'shifter-minor-mode-hist s))
-      (funcall s 1))))
-
-;;;###autoload
-(defun shifter-turn-off-minor-mode (force)
-  "Turn off minor mode.
-If FORCE is non-nil, read and disable minor mode regardless
-of its state."
-  (interactive "P")
-  (let ((l (if force
-               (fmmm-minor-mode-list)
-             (fmmm-enabled-minor-mode-list)))
-        (h (funcall (if force
-                        'identity
-                      (lambda (l)
-                        (cl-remove-if
-                         (lambda (s) (memq s (fmmm-disabled-minor-mode-list)))
-                         l)))
-                    shifter-minor-mode-hist)))
-    (mapc (lambda (s) (setq l (delq s l))) h)
-    (let ((s (intern (completing-read "Minor mode: "
-                                      (mapcar 'symbol-name (append h l))))))
-      (let ((f (symbol-function s)))
-        (if (autoloadp f) (autoload-do-load f)))
-      (if (not (fmmm-minor-mode-p s))
-          (setq s 'ignore))
-      (when (fmmm-minor-mode-p s)
-        (setq shifter-minor-mode-hist (delq s shifter-minor-mode-hist))
-        (add-to-list 'shifter-minor-mode-hist s))
-      (funcall s -1))))
+      (funcall s shift))))
 
 ;;;###autoload
 (define-minor-mode shifter-non-volatile-hist-mode
