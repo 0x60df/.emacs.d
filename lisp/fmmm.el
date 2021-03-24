a05011aae19ae813ec87dbe997470fb9e0b0e444
0x60df <0x60df@gmail.com>
2021-03-25 00:11:08 +0900

    Modify user feature fmmm
    Refine codes for minor mode.

--- lisp/fmmm.el
+++ lisp/fmmm.el
@@ -10,30 +10,15 @@
   "Feature for major/minor-mode"
   :group 'emacs)
 
-(defcustom fmmm-complementary-minor-mode-list nil
-  "List of simbols which are considered as `minor-mode' by `fmmm'."
-  :type '(repeat symbol)
-  :group 'fmmm)
-
 (defcustom fmmm-cache-file (concat user-emacs-directory "fmmm-cache")
   "File which stores fmmm cache."
   :group 'fmmm
   :type 'file)
 
-(defvar fmmm-minor-mode-variable-alist
-  (let (l)
-    (mapatoms (lambda (a)
-                (if (symbolp a)
-                    (let ((minor-mode-function (get a :minor-mode-function)))
-                      (if minor-mode-function
-                          (setq l (cons (cons minor-mode-function a) l)))))))
-    l)
-  "Alist of minor-mode and minor-mode variable.")
-
 (defvar fmmm--major-mode-on-autoload-list nil
   "List of simbols of `major-mode' which will be autoloaded.")
 
-(defvar fmmm-minor-mode-on-autoload-list nil
+(defvar fmmm--minor-mode-on-autoload-list nil
   "List of simbols of `minor-mode' which will be autoloaded.")
 
 (defun fmmm-save-cache ()
@@ -46,8 +31,8 @@
            (current-buffer))
     (prin1 `(mapc (lambda (m)
                     (if (symbol-function m)
-                        (push m fmmm-minor-mode-on-autoload-list)))
-                  ',(reverse fmmm-minor-mode-on-autoload-list))
+                        (push m fmmm--minor-mode-on-autoload-list)))
+                  ',(reverse fmmm--minor-mode-on-autoload-list))
            (current-buffer))
   (write-file fmmm-cache-file)))
 
@@ -84,95 +69,76 @@ major mode."
 
 ;;;###autoload
 (defun fmmm-major-mode-list ()
-  "Return list consist of symbol which seem to be major mode."
+  "Return list consist of symbol which seems to be major mode."
   (let (l)
     (mapatoms (lambda (a) (if (fmmm-major-mode-p a) (push a l))))
     l))
 
 ;;;###autoload
 (defun fmmm-minor-mode-p (symbol)
-  "Non-nil if SYMBOL seems to be minor mode."
-  (letrec ((root-symbol
-            (lambda (s)
-              (let ((f (symbol-function s)))
-                (cond ((null f) s)
-                      ((symbolp f) (funcall root-symbol f))
-                      (t s))))))
-    (let ((root (funcall root-symbol symbol)))
-      (or (memq root minor-mode-list)
-          (assq root minor-mode-alist)
-          (assq root minor-mode-map-alist)))))
+  "Non-nil if SYMBOL seems to be minor mode.
+This function obly can predicate loaded functions, in other
+words, this function regard autoload as non major mode.
+That's because minor mode symbol does not listed in any of
+`minor-mode-list', `minor-mode-alist' and
+`minor-mode-map-alist'."
+  (or (memq symbol fmmm--minor-mode-on-autoload-list)
+      (letrec ((inspect
+                (lambda (s)
+                  (let ((f (symbol-function s)))
+                    (cond ((null f) s)
+                          ((symbolp f) (funcall inspect f))
+                          (t s))))))
+        (let ((root (funcall inspect symbol)))
+          (or (memq root minor-mode-list)
+              (assq root minor-mode-alist)
+              (assq root minor-mode-map-alist))))))
 
 ;;;###autoload
 (defun fmmm-minor-mode-list ()
-  "Return list consist of minor mode symbol."
-  (let ((valid-complemntary-list
-         (letrec ((filter
-                   (lambda (p l)
-                     (cond ((null l) l)
-                           ((funcall p (car l)) (funcall filter p (cdr l)))
-                           (t (cons (car l) (funcall filter p (cdr l)))))))
-                  (inspect
-                   (lambda (s)
-                     (let ((f (symbol-function s)))
-                       (cond ((null f) nil)
-                             ((symbolp f) (funcall inspect f))
-                             (t f))))))
-           (funcall filter
-                    (lambda (symbol)
-                      (and (not (autoloadp (funcall inspect symbol)))
-                           (not (fmmm-minor-mode-p symbol))))
-                    fmmm-complementary-minor-mode-list))))
-    (let ((l (copy-tree minor-mode-list)))
-      (mapc (lambda (s) (setq l (delq s l))) fmmm-complementary-minor-mode-list)
-      (mapc (lambda (s) (setq l (delq s l))) fmmm-minor-mode-on-autoload-list)
-      (append valid-complemntary-list fmmm-minor-mode-on-autoload-list l))))
+  "Return list consist of symbol which seems to be minor mode."
+  (let (l)
+    (mapatoms (lambda (a) (if (fmmm-minor-mode-p a) (push a l))))
+    l))
 
-;;;###autoload
-(defun fmmm-enabled-minor-mode-list ()
-  "Return list consist of enabled minor mode symbol."
-  (fmmm-update-minor-mode-variable-alist)
-  (letrec ((filter (lambda (p l)
-                     (cond ((null l) l)
-                           ((funcall p (car l)) (funcall filter p (cdr l)))
-                           (t (cons (car l) (funcall filter p (cdr l))))))))
-    (funcall filter (lambda (s)
-                      (let* ((cell (assq s fmmm-minor-mode-variable-alist))
-                             (state (if cell (cdr cell) s)))
-                        (not (and (boundp state)
-                                  (symbol-value state)))))
-             (fmmm-minor-mode-list))))
-
-;;;###autoload
-(defun fmmm-disabled-minor-mode-list ()
-  "Return list consist of disabled minor mode symbol."
-  (fmmm-update-minor-mode-variable-alist)
-  (letrec ((filter (lambda (p l)
-                     (cond ((null l) l)
-                           ((funcall p (car l)) (funcall filter p (cdr l)))
-                           (t (cons (car l) (funcall filter p (cdr l))))))))
-    (funcall filter (lambda (s)
-                      (let* ((cell (assq s fmmm-minor-mode-variable-alist))
-                             (state (if cell (cdr cell) s)))
-                        (and (boundp state)
-                             (symbol-value state))))
-             (fmmm-minor-mode-list))))
-
-(defun fmmm-update-minor-mode-variable-alist ()
-  "Update `fmmm-minor-mode-variable-alist'.
-according to current `obarray'"
+(defun fmmm--minor-mode-variable-alist ()
+  "Return alist of minor-mode and minor-mode variable.
+Returned list only contains minor mode whose mode function
+symbol is different from mode variable one."
   (let (l)
     (mapatoms (lambda (a)
                 (if (symbolp a)
                     (let ((minor-mode-function (get a :minor-mode-function)))
                       (if minor-mode-function
-                          (setq l (cons (cons minor-mode-function a) l)))))))
-    (setq fmmm-minor-mode-variable-alist l)))
+                          (push (cons minor-mode-function a) l))))))
+    l))
+
+;;;###autoload
+(defun fmmm-enabled-minor-mode-list ()
+  "Return list of symbol which seems to be enabled minor mode."
+  (let ((minor-mode-variable-alist (fmmm--minor-mode-variable-alist)))
+    (seq-filter (lambda (s)
+                  (let* ((cell (assq s minor-mode-variable-alist))
+                         (state (if cell (cdr cell) s)))
+                    (and (boundp state)
+                         (symbol-value state))))
+                (fmmm-minor-mode-list))))
+
+;;;###autoload
+(defun fmmm-disabled-minor-mode-list ()
+  "Return list of symbol which seems to be disabled minor mode."
+  (let ((minor-mode-variable-alist (fmmm--minor-mode-variable-alist)))
+    (seq-filter (lambda (s)
+                  (let* ((cell (assq s minor-mode-variable-alist))
+                         (state (if cell (cdr cell) s)))
+                    (not (and (boundp state)
+                              (symbol-value state)))))
+                (fmmm-minor-mode-list))))
 
 (defconst fmmm--initial-major-mode-list (fmmm-major-mode-list)
   "List of simbols of `major-mode' which have been loaded on loading.")
 
-(defconst fmmm-initial-minor-mode-list (fmmm-minor-mode-list)
+(defconst fmmm--initial-minor-mode-list (fmmm-minor-mode-list)
   "List of simbols of `minor-mode' which have been loaded on loading.")
 
 (defun fmmm-update-major-mode-on-autoload-list ()
@@ -188,23 +154,23 @@ via `obarray'."
              (push a fmmm--major-mode-on-autoload-list))))))
 
 (defun fmmm-update-minor-mode-on-autoload-list ()
-  "Update `fmmm-minor-mode-on-autoload-list'.
-according to current `obarray'"
+  "Update `fmmm--minor-mode-on-autoload-list'.
+according to currently loaded functions which are reachable
+via `obarray'."
   (mapatoms
    (lambda (a)
      (if (symbolp a)
          (if (and (fmmm-minor-mode-p a)
-                  (not (memq a fmmm-initial-minor-mode-list))
-                  (not (memq a fmmm-minor-mode-on-autoload-list)))
-             (setq fmmm-minor-mode-on-autoload-list
-                   (cons a fmmm-minor-mode-on-autoload-list)))))))
+                  (not (memq a fmmm--initial-minor-mode-list))
+                  (not (memq a fmmm--minor-mode-on-autoload-list)))
+             (push a fmmm--minor-mode-on-autoload-list))))))
 
 ;;;###autoload
 (define-minor-mode fmmm-autoload-collector-mode
   "Minor mode for supporting fmmm autoload collecting system.
 When enabled, load `fmmm-cache-file', if
 `fmmm--major-mode-on-autoload-list', and
-`fmmm-minor-mode-on-autoload-list' are nil.
+`fmmm--minor-mode-on-autoload-list' are nil.
 In addition add hook
 `fmmm-update-major-mode-on-autoload-list',
 `fmmm-update-minor-mode-on-autoload-list',
@@ -214,7 +180,7 @@ and `fmmm-save-cache' to `kill-meacs-hook'"
   (if fmmm-autoload-collector-mode
       (progn
         (if (and (null fmmm--major-mode-on-autoload-list)
-                 (null fmmm-minor-mode-on-autoload-list))
+                 (null fmmm--minor-mode-on-autoload-list))
             (load fmmm-cache-file t t t))
         (add-hook 'kill-emacs-hook #'fmmm-save-cache)
         (add-hook 'kill-emacs-hook
@@ -225,7 +191,7 @@ and `fmmm-save-cache' to `kill-meacs-hook'"
     (fmmm-update-minor-mode-on-autoload-list)
     (fmmm-save-cache)
     (setq fmmm--major-mode-on-autoload-list nil
-          fmmm-minor-mode-on-autoload-list nil)
+          fmmm--minor-mode-on-autoload-list nil)
     (remove-hook 'kill-emacs-hook #'fmmm-save-cache)
     (remove-hook 'kill-emacs-hook
                  #'fmmm-update-minor-mode-on-autoload-list)
