f95c86d5ead236ffeed37cf55dfbfc316b0bb246
0x60df <0x60df@gmail.com>
2021-08-09 19:27:53 +0900

    Add unit init-ispell

---
+++ init.d/lisp/features/init-ispell.el
@@ -0,0 +1,14 @@
+
+;;;; init-ispell.el
+
+
+(premise init)
+
+(with-eval-after-load 'ispell
+  (advice-add 'ispell-word :before
+              (lambda (&rest args)
+                (with-current-buffer (get-buffer-create ispell-choices-buffer)
+                  (setq show-trailing-whitespace nil)))))
+
+
+(resolve init-ispell)
