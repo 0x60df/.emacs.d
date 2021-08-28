d4dae91454056fd8ca7b7f97bd3821fec8d774a4
0x60df <0x60df@gmail.com>
2021-08-29 03:10:55 +0900

    Modify unit init-yester-theme
    Remove unnecessary forms.

--- init.d/lisp/user-packages/init-yester-theme.el
+++ init.d/lisp/user-packages/init-yester-theme.el
@@ -5,15 +5,7 @@
 (premise init)
 (premise inst-yester-theme)
 
-(eval-when-compile (require 'yester))
-
 (declare-function yester-phase-shift-mode "yester")
-(declare-function yester-recalc "yester")
-
-(declare-function yester--recalc-once-on-post-command-for-accessory
-                  load-file-name t t)
-(declare-function yester--background-mode-guard-for-accessory
-                  load-file-name t t)
 
 (with-eval-after-load 'yester
   (yester-phase-shift-mode))
