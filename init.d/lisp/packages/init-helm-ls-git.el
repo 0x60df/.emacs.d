758090fc44537db5450ff58409c870f90ee017ee
0x60df <0x60df@gmail.com>
2021-10-31 16:39:49 +0900

    Modify unit init-helm-ls-git
    Follow upstream.

--- init.d/lisp/packages/init-helm-ls-git.el
+++ init.d/lisp/packages/init-helm-ls-git.el
@@ -3,11 +3,15 @@
 
 
 (premise init)
+(premise advice)
 (premise init-helm)
 (premise inst-helm-ls-git)
 
+(advice-add-for-once 'helm-browse-project :before
+                     (lambda (&rest _args)
+                       (require 'helm-ls-git)))
 (with-eval-after-load 'helm-global-bindings
- (define-key helm-command-map (kbd "C-v") #'helm-ls-git-ls))
+ (define-key helm-command-map (kbd "C-v") #'helm-browse-project))
 
 
 (resolve init-helm-ls-git)
