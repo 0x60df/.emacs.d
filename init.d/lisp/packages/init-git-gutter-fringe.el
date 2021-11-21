77b7a73d0c62a929a28593ee7d8c6e583e0fd84e
0x60df <0x60df@gmail.com>
2021-11-21 15:58:33 +0900

    Modify unit init-git-gutter-fringe
    Support direct starting graphic emacs.

--- init.d/lisp/packages/init-git-gutter-fringe.el
+++ init.d/lisp/packages/init-git-gutter-fringe.el
@@ -7,14 +7,17 @@
 (premise frame)
 (premise inst-git-gutter-fringe)
 
-(add-hook-for-once 'after-make-terminal-functions
-                   (lambda (_)
-                     (if (and (boundp 'git-gutter-mode) git-gutter-mode)
-                         (require 'git-gutter-fringe)
-                       (advice-add-for-once
-                        'git-gutter-mode
-                        :before
-                        (lambda (&rest args) (require 'git-gutter-fringe))))))
+(let ((require-fringe
+       (lambda (&optional _)
+         (if (and (boundp 'git-gutter-mode) git-gutter-mode)
+             (require 'git-gutter-fringe)
+           (advice-add-for-once
+            'git-gutter-mode
+            :before
+            (lambda (&rest args) (require 'git-gutter-fringe)))))))
+  (cond  ((display-graphic-p) (funcall require-fringe))
+         ((daemonp)
+          (add-hook-for-once 'after-make-terminal-functions require-fringe))))
 
 
 (resolve init-git-gutter-fringe)
