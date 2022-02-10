8ee4ef236361f9885cc957a601c858bc1121e26d
0x60df <0x60df@gmail.com>
2022-02-10 23:43:16 +0900

    Modify unit init-el-get and delete files for el-get

--- init.d/lisp/packages/init-el-get.el
+++ init.d/lisp/packages/init-el-get.el
@@ -6,10 +6,6 @@
 (premise custom)
 (premise inst-el-get)
 
-(with-eval-after-load 'el-get-recipes
-  (add-to-list 'el-get-recipe-path
-               (concat user-emacs-directory "el-get-user/site-recipes")))
-
 (with-eval-after-load 'el-get-list-packages
   (add-hook 'el-get-package-menu-mode-hook
             (lambda () (setq show-trailing-whitespace nil))))
