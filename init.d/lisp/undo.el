d9de79e2910ad0c35737da8ff1b76b27eb7b5db8
0x60df <0x60df@gmail.com>
2022-04-18 21:40:02 +0900

    Modify unit undo
    Push head of buffer-undo-list to undo-turning-point-list when any
    turning point is pop.

--- init.d/lisp/undo.el
+++ init.d/lisp/undo.el
@@ -21,18 +21,7 @@
                       (unless (eq last-command 'undo)
                         (let ((turning-point (cdr-safe buffer-undo-list)))
                           (when (consp turning-point)
-                            (push turning-point undo-turning-point-list)
-                            (setq undo-turning-point-list
-                                  (seq-filter
-                                   (lambda (turning-point)
-                                     (and (consp buffer-undo-list)
-                                          (let ((rest buffer-undo-list))
-                                            (while (and rest
-                                                        (not (eq turning-point
-                                                                 rest)))
-                                              (setq rest (cdr rest)))
-                                            rest)))
-                                   undo-turning-point-list)))))))
+                            (push turning-point undo-turning-point-list))))))
 
 (advice-add 'undo
             :after (lambda (&rest _)
@@ -49,7 +38,20 @@
                                                      undo-standard-cursor-color)
                                 (setq undo-standard-cursor-color nil))))
                            (set-frame-parameter
-                            nil 'cursor-color undo-turning-point-cursor-color))
+                            nil 'cursor-color undo-turning-point-cursor-color)
+                           (when (consp buffer-undo-list)
+                             (push buffer-undo-list undo-turning-point-list)
+                             (setq undo-turning-point-list
+                                   (seq-filter
+                                    (lambda (turning-point)
+                                      (and (consp buffer-undo-list)
+                                           (let ((rest buffer-undo-list))
+                                             (while (and rest
+                                                         (not (eq turning-point
+                                                                  rest)))
+                                               (setq rest (cdr rest)))
+                                             rest)))
+                                    undo-turning-point-list))))
                        (when undo-standard-cursor-color
                          (set-frame-parameter nil 'cursor-color
                                               undo-standard-cursor-color)
