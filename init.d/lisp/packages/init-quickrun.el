e06b857e82a053faab38b1a429d36b9881a945d8
0x60df <0x60df@gmail.com>
2022-06-19 20:24:58 +0900

    Modify unit init-quickrun
    Modify bindings for balance-weight-mode.

--- init.d/lisp/packages/init-quickrun.el
+++ init.d/lisp/packages/init-quickrun.el
@@ -3,12 +3,42 @@
 
 
 (premise init)
+(premise bindings)
 (premise inst-quickrun)
 
 (eval-when-compile (require 'ruby-mode))
 
 (with-eval-after-load 'ruby-mode
   (define-key ruby-mode-map (kbd "C-c c q") #'quickrun))
+
+(advice-add 'balance-mode-context :filter-return
+            (lambda (ret)
+              (if (eq major-mode 'quickrun--mode) #'balance-weight-mode ret)))
+
+(add-hook 'balance-mode-update-keys-hook
+          (lambda ()
+            (when (eq major-mode 'quickrun--mode)
+              (balance-mode-implement-keys
+               (list (kbd "C-SPC")
+                     (kbd "C-@")
+                     (kbd "C-n")
+                     (kbd "C-p")
+                     (kbd "C-f")
+                     (kbd "C-b")
+                     (kbd "C-a")
+                     (kbd "C-e")
+                     (kbd "C-v")
+                     (kbd "C-s")
+                     (kbd "C-r")
+                     (kbd "C-l a")
+                     (kbd "C-l e"))
+               overriding-balance-weight-mode-map)
+              (balance-mode-alias-keys
+               (list `(,(kbd "l SPC a") . ,(kbd "l a"))
+                     `(,(kbd "l SPC e") . ,(kbd "l e")))
+               overriding-balance-weight-mode-map)
+              (define-key overriding-balance-weight-mode-map (kbd "W")
+                #'kill-ring-save))))
 
 
 (resolve init-quickrun)
