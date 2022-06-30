09770206f817e2308ef036a0d3587956ff875c31
0x60df <0x60df@gmail.com>
2022-06-30 23:58:55 +0900

    Modify unit init-vertico
    Modify settings for completion-in-region-function.

--- init.d/lisp/packages/init-vertico.el
+++ init.d/lisp/packages/init-vertico.el
@@ -6,7 +6,16 @@
 (premise inst-vertico)
 
 (add-hook 'emacs-startup-hook #'vertico-mode)
- (setq enable-recursive-minibuffers t)
+(setq enable-recursive-minibuffers t)
+
+(with-eval-after-load 'consult
+  (setq completion-in-region-function
+        (lambda (&rest args)
+          (apply (if vertico-mode
+                     #'consult-completion-in-region
+                   #'#'completion--in-region)
+                 args))))
+
 
 
 (resolve init-vertico)
