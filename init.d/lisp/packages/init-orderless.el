5d7dff595952091b46664eab48ade0d6dad4a18d
0x60df <0x60df@gmail.com>
2022-10-08 01:26:54 +0900

    Modify unit init-orderless
    Modify style dispatchers.

--- init.d/lisp/packages/init-orderless.el
+++ init.d/lisp/packages/init-orderless.el
@@ -8,9 +8,16 @@
 
 (setq completion-category-defaults nil)
 
+(defun orderless-without-if-bang (pattern _index _total)
+  (cond
+   ((equal "!" pattern) '(orderless-literal . ""))
+   ((string-prefix-p "!" pattern)
+    `(orderless-without-literal . ,(substring pattern 1)))))
+
 (custom-set-variables
  '(completion-styles '(orderless basic))
- '(completion-category-overrides))
+ '(completion-category-overrides)
+ '(orderless-style-dispatchers '(orderless-without-if-bang)))
 
 
 (resolve init-orderless)
