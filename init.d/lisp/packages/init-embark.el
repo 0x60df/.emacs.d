de79740e4cc7d77bee54afdadbc1317d8bc0b53f
0x60df <0x60df@gmail.com>
2022-10-14 00:34:14 +0900

    Modify unit init-embark
    Add a binding for balance-mode.

--- init.d/lisp/packages/init-embark.el
+++ init.d/lisp/packages/init-embark.el
@@ -19,6 +19,26 @@
 
 (add-to-list 'balance-mode-key-alias-alist `(,(kbd "q SPC h") . ,(kbd "q h")))
 
+(add-hook 'balance-mode-update-keys-hook
+            (lambda ()
+              (when (or (string-equal (buffer-name) "*Messages*")
+                        (eq major-mode #'help-mode)
+                        (eq major-mode 'emacs-lisp-compilation-mode)
+                        (eq major-mode #'dired-mode)
+                        (eq major-mode 'org-agenda-mode)
+                        (eq major-mode 'magit-status-mode))
+                (let ((entry (lookup-key (current-local-map) (kbd "q"))))
+                  (if (and entry (not (numberp entry)))
+                      (define-key overriding-balance-weight-mode-map
+                        (kbd "qq") entry)))
+
+                (balance-mode-implement-keys
+                 (list (kbd "C-q h b"))
+                 overriding-balance-weight-mode-map)
+                (balance-mode-alias-keys
+                 `((,(kbd "q SPC h b") . ,(kbd "q h b")))
+               overriding-balance-weight-mode-map))))
+
 (setq prefix-help-command #'embark-prefix-help-command)
 
 (add-to-list 'display-buffer-alist
