cf40721e5cac210febdc9b8e79087d038982583e
0x60df <0x60df@gmail.com>
2022-12-03 17:12:30 +0900

    Modify unit init-vertico
    Prevent sort for loophole-commands.

--- init.d/lisp/packages/init-vertico.el
+++ init.d/lisp/packages/init-vertico.el
@@ -8,14 +8,34 @@
 (add-hook 'emacs-startup-hook #'vertico-mode)
 (setq enable-recursive-minibuffers t)
 
+(add-hook 'emacs-startup-hook #'vertico-multiform-mode)
+(with-eval-after-load 'vertico-multiform
+  (dolist (command '(loophole-register
+                     loophole-unregister
+                     loophole-prioritize
+                     loophole-globalize
+                     loophole-localize
+                     loophole-enable
+                     loophole-disable
+                     loophole-name
+                     loophole-tag
+                     loophole-start-editing
+                     loophole-start-timer
+                     loophole-stop-timer
+                     loophole-extend-timer
+                     loophole-describe
+                     loophole-merge
+                     loophole-duplicate))
+    (add-to-list 'vertico-multiform-commands
+                 `(,command (vertico-sort-function . identity)))))
+
 (with-eval-after-load 'consult
   (setq completion-in-region-function
         (lambda (&rest args)
           (apply (if vertico-mode
                      #'consult-completion-in-region
-                   #'#'completion--in-region)
+                   #'completion--in-region)
                  args))))
-
 
 
 (resolve init-vertico)
