a0026696dfbc64a5461188a77a9c547891fffd9b
0x60df <0x60df@gmail.com>
2022-12-15 23:51:22 +0900

    Modify unit init-wdired

--- init.d/lisp/features/init-wdired.el
+++ init.d/lisp/features/init-wdired.el
@@ -16,7 +16,6 @@
                                     (progn
                                       (balance-weight-mode 0)
                                       (balance-mode 0)
-                                      (echo (key-binding (kbd "C-f")))
                                       (balance-mode-implement-keys
                                        (list (kbd "C-c C-c")
                                              (kbd "C-c C-k")
@@ -29,26 +28,15 @@
                                       (balance-mode-implement-keys
                                        (list (kbd "C-c C-c")
                                              (kbd "C-c C-k"))
-                                       overriding-balance-mode-map))
-                                  (dolist (key (list (kbd "c c")
-                                                     (kbd "c k")))
-                                    (define-key
-                                      overriding-balance-mode-map
-                                      key
-                                      nil))
-                                  (dolist (key (list (kbd "c c")
-                                                     (kbd "c k")
-                                                     (kbd "f")
-                                                     (kbd "b")
-                                                     (kbd "n")
-                                                     (kbd "p")
-                                                     (kbd "e")))
-                                    (define-key
-                                      overriding-balance-weight-mode-map
-                                      key
-                                      nil))
-                                  (balance-mode 0)
-                                  (balance-weight-mode 1)))))
+                                       overriding-balance-mode-map)))))
+  (advice-add 'wdired-finish-edit :after (lambda (&rest _)
+                                           (balance-mode-update-keys)
+                                           (balance-mode 0)
+                                            (balance-weight-mode 1)))
+  (advice-add 'wdired-abort-changes :after (lambda (&rest _)
+                                             (balance-mode-update-keys)
+                                             (balance-mode 0)
+                                             (balance-weight-mode 1))))
 
 
 (resolve init-wdired)
