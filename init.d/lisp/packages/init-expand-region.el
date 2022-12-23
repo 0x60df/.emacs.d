16dbfe0090a8ad6045607e6b5968b1e7b56fb0c6
0x60df <0x60df@gmail.com>
2022-12-24 00:52:20 +0900

    Modify unit init-expand-region

--- init.d/lisp/packages/init-expand-region.el
+++ init.d/lisp/packages/init-expand-region.el
@@ -8,20 +8,11 @@
 
 (declare-function er/contract-region "expand-region")
 
-(defun er/manipulate-region (arg)
-  "Expand or contract region by semantic region.
-ARG acts same as `er/expand-region'."
-  (interactive "p")
-  (funcall-interactively #'er/expand-region arg)
-  (catch 'quit
-    (while t
-      (let ((key (read-key "Manipulating region...")))
-        (cond ((eql key ?@) (funcall-interactively #'er/expand-region arg))
-              ((eql key ?`) (funcall-interactively #'er/contract-region arg))
-              (t (setq unread-command-events (list key)) (throw 'quit t)))))))
+(custom-set-variables
+ '(expand-region-contract-fast-key "`"))
 
 (overriding-set-key (kbd "C-`") #'er/expand-region)
-(overriding-set-key (kbd "M-@") #'er/manipulate-region)
+(overriding-set-key (kbd "M-@") #'er/expand-region)
 (with-eval-after-load 'expand-region
   (overriding-set-key (kbd "C-M-`") #'er/contract-region))
 
