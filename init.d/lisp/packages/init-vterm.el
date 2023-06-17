d4a5d4d5a0d1e8dfd3d46c1db52fc82ea8598cbc
0x60df <0x60df@gmail.com>
2023-06-18 00:39:55 +0900

    Modify unit init-vterm
    Add minor-mode to let C-o key go to shell.

--- init.d/lisp/packages/init-vterm.el
+++ init.d/lisp/packages/init-vterm.el
@@ -9,14 +9,20 @@
 
 (declare-function vterm-send "vterm")
 
+(define-minor-mode overriding-vterm-ctl-o-mode
+  "Minor mode to enable overriding keymap for C-o."
+  :keymap `((,(kbd "C-o") . ,(lambda () (interactive) (vterm-send "C-o")))))
+
 (with-eval-after-load 'vterm
   (add-hook 'vterm-mode-hook (lambda () (setq show-trailing-whitespace nil)))
   (add-hook 'vterm-mode-hook (lambda () (setq-local global-hl-line-mode nil)))
   (if (init-unit-p bindings)
       (add-hook 'vterm-mode-hook (lambda () (balance-mode 0))))
 
-  (define-key vterm-mode-map (kbd "C-@")
-    (lambda () (interactive) (vterm-send "C-SPC")))
+  (push `(overriding-vterm-ctl-o-mode . ,overriding-vterm-ctl-o-mode-map)
+        overriding-reserved-key-map-alist)
+  (add-hook 'vterm-mode-hook #'overriding-vterm-ctl-o-mode)
+
   (define-key vterm-mode-map (kbd "C-g")
     (lambda () (interactive) (vterm-send "C-g"))))
 
