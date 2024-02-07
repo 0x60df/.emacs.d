becb8ae6a5a8eda34d2b75c66ab2add80c2c9fc4
0x60df <0x60df@gmail.com>
2024-02-07 21:45:01 +0900

    Modify unit init-wdired
    Disable balance-weight-mode for wdired.

--- init.d/lisp/features/init-wdired.el
+++ init.d/lisp/features/init-wdired.el
@@ -11,18 +11,14 @@
   (define-key dired-mode-map "w" #'wdired-change-to-wdired-mode))
 
 (with-eval-after-load 'wdired
+  (advice-add 'balance-mode-context :filter-return
+              (lambda (ret)
+                (if (eq major-mode 'wdired-mode)
+                    #'balance-mode
+                  ret)))
   (add-hook 'wdired-mode-hook (lambda ()
                                 (balance-weight-mode 0)
-                                (balance-mode 0)
-                                (balance-mode-implement-keys
-                                 (list (kbd "C-c C-c")
-                                       (kbd "C-c C-k")
-                                       (kbd "C-f")
-                                       (kbd "C-b")
-                                       (kbd "C-n")
-                                       (kbd "C-p")
-                                       (kbd "C-e"))
-                                 overriding-balance-weight-mode-map)
+                                (balance-mode 1)
                                 (balance-mode-implement-keys
                                  (list (kbd "C-c C-c")
                                        (kbd "C-c C-k"))
@@ -30,7 +26,7 @@
   (advice-add 'wdired-finish-edit :after (lambda (&rest _)
                                            (balance-mode-update-keys)
                                            (balance-mode 0)
-                                            (balance-weight-mode 1)))
+                                           (balance-weight-mode 1)))
   (advice-add 'wdired-abort-changes :after (lambda (&rest _)
                                              (balance-mode-update-keys)
                                              (balance-mode 0)
